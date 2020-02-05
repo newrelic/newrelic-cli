e26a25149a07f54290f356943029e9bc42aca862
Zach Leslie <zleslie@newrelic.com>
2020-02-05 10:59:49 -0800

    feat(entities): implement entities tag and tag value deletion
    resolves: #18

--- internal/entities/command.go
+++ internal/entities/command.go
@@ -3,6 +3,7 @@ package entities
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	prettyjson "github.com/hokaccha/go-prettyjson"
 	"github.com/spf13/cobra"
@@ -12,9 +13,11 @@ import (
 )
 
 var (
-	nrClient   *newrelic.NewRelic
-	entityName string
-	entityGUID string
+	nrClient     *newrelic.NewRelic
+	entityName   string
+	entityGUID   string
+	entityTags   []string
+	entityValues []string
 )
 
 // SetClient is the API for passing along the New Relic client to this command
@@ -45,14 +48,13 @@ var entitiesSearchCmd = &cobra.Command{
 		params := entities.SearchEntitiesParams{
 			Name: entityName,
 		}
-		entities, err := nrClient.Entities.SearchEntities(params)
 
+		entities, err := nrClient.Entities.SearchEntities(params)
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		json, err := prettyjson.Marshal(entities)
-
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -70,13 +72,11 @@ var entitiesDescribeTags = &cobra.Command{
 		}
 
 		tags, err := nrClient.Entities.ListTags(entityGUID)
-
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		json, err := prettyjson.Marshal(tags)
-
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -85,11 +85,69 @@ var entitiesDescribeTags = &cobra.Command{
 	},
 }
 
+var entitiesDeleteTags = &cobra.Command{
+	Use:   "delete-tags",
+	Short: "delete tags",
+	Run: func(cmd *cobra.Command, args []string) {
+		if nrClient == nil {
+			log.Fatal("missing New Relic client configuration")
+		}
+
+		err := nrClient.Entities.DeleteTags(entityGUID, entityTags)
+		if err != nil {
+			log.Fatal(err)
+		}
+	},
+}
+
+var entitiesDeleteTagValues = &cobra.Command{
+	Use:   "delete-tag-values",
+	Short: "delete-tag-values",
+	Run: func(cmd *cobra.Command, args []string) {
+		if nrClient == nil {
+			log.Fatal("missing New Relic client configuration")
+		}
+
+		var tagValues []entities.TagValue
+
+		for _, x := range entityValues {
+			if !strings.Contains(x, ":") {
+				log.Fatal("Tag values must be specified as colon seperated key:value pairs")
+			}
+
+			v := strings.SplitN(x, ":", 2)
+
+			tv := entities.TagValue{
+				Key:   v[0],
+				Value: v[1],
+			}
+			tagValues = append(tagValues, tv)
+		}
+
+		err := nrClient.Entities.DeleteTagValues(entityGUID, tagValues)
+		if err != nil {
+			log.Fatal(err)
+		}
+	},
+}
+
 func init() {
 	Command.AddCommand(entitiesSearchCmd)
-	entitiesSearchCmd.Flags().StringVarP(&entityName, "name", "n", "ENTITY_NAME", "entity name")
+	entitiesSearchCmd.Flags().StringVarP(&entityName, "name", "n", "ENTITY_NAME", "name of the entity to search")
 
 	Command.AddCommand(entitiesDescribeTags)
-	entitiesDescribeTags.Flags().StringVarP(&entityGUID, "guid", "g", "", "entity guid")
+	entitiesDescribeTags.Flags().StringVarP(&entityGUID, "guid", "g", "", "entity GUID to describe")
 	entitiesDescribeTags.MarkFlagRequired("guid")
+
+	Command.AddCommand(entitiesDeleteTags)
+	entitiesDeleteTags.Flags().StringVarP(&entityGUID, "guid", "g", "", "entity GUID to delete tags on")
+	entitiesDeleteTags.Flags().StringSliceVarP(&entityTags, "tags", "t", []string{}, "tag names to delete from the entity")
+	entitiesDeleteTags.MarkFlagRequired("guid")
+	entitiesDeleteTags.MarkFlagRequired("tags")
+
+	Command.AddCommand(entitiesDeleteTagValues)
+	entitiesDeleteTagValues.Flags().StringVarP(&entityGUID, "guid", "g", "", "entity GUID to delete tag values on")
+	entitiesDeleteTagValues.Flags().StringSliceVarP(&entityValues, "value", "v", []string{}, "keyy:value tags to delete from the entity")
+	entitiesDeleteTagValues.MarkFlagRequired("guid")
+	entitiesDeleteTagValues.MarkFlagRequired("value")
 }
--- internal/entities/command_test.go
+++ internal/entities/command_test.go
@@ -8,6 +8,49 @@ import (
 )
 
 func TestEntitiesDescribeTags(t *testing.T) {
-	r := entitiesDescribeTags.Flag("guid")
-	assert.Equal(t, []string{"true"}, r.Annotations[cobra.BashCompOneRequiredFlag])
+	assert.Equal(t, "describe-tags", entitiesDescribeTags.Name())
+
+	requiredFlags := []string{"guid"}
+
+	for _, r := range requiredFlags {
+		x := entitiesDescribeTags.Flag(r)
+		if x == nil {
+			t.Errorf("Missing required flag: %s\n", r)
+			continue
+		}
+
+		assert.Equal(t, []string{"true"}, x.Annotations[cobra.BashCompOneRequiredFlag])
+	}
+}
+
+func TestEntitiesDeleteTags(t *testing.T) {
+	assert.Equal(t, "delete-tags", entitiesDeleteTags.Name())
+
+	requiredFlags := []string{"guid", "tags"}
+
+	for _, r := range requiredFlags {
+		x := entitiesDeleteTags.Flag(r)
+		if x == nil {
+			t.Errorf("Missing required flag: %s\n", r)
+			continue
+		}
+
+		assert.Equal(t, []string{"true"}, x.Annotations[cobra.BashCompOneRequiredFlag])
+	}
+}
+
+func TestEntitiesDeleteTagValues(t *testing.T) {
+	assert.Equal(t, "delete-tag-values", entitiesDeleteTagValues.Name())
+
+	requiredFlags := []string{"guid", "value"}
+
+	for _, r := range requiredFlags {
+		x := entitiesDeleteTagValues.Flag(r)
+		if x == nil {
+			t.Errorf("Missing required flag: %s\n", r)
+			continue
+		}
+
+		assert.Equal(t, []string{"true"}, x.Annotations[cobra.BashCompOneRequiredFlag])
+	}
 }
