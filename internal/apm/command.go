6b246b2274b2bfbf39970da5b8307bd1dad6b6cf
Zach Leslie <zleslie@newrelic.com>
2020-03-02 13:06:38 -0800

    feat(apm): include get command for APM applications
    resolves: #79

--- internal/apm/command.go
+++ internal/apm/command.go
@@ -7,20 +7,32 @@ import (
 	"github.com/newrelic/newrelic-cli/internal/client"
 	"github.com/newrelic/newrelic-client-go/newrelic"
 	"github.com/newrelic/newrelic-client-go/pkg/apm"
+	"github.com/newrelic/newrelic-client-go/pkg/entities"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 var (
-	apmApplicationID   int
-	deploymentRevision string
-	deploymentID       int
+	apmApplicationAccountID string
+	apmApplicationID        int
+	apmApplicationName      string
+	apmApplicationGUID      string
+	deploymentRevision      string
+	deploymentID            int
 )
 
 // Command represents the apm command
 var Command = &cobra.Command{
 	Use:   "apm",
 	Short: "Subcommands to interact with New Relic APM",
+}
+
+// Command represents the apm command
+var apmApplication = &cobra.Command{
+	Use:     "application",
+	Short:   "Subcommands to interact with New Relic APM applications",
+	Example: "newrelic apm application --help",
+	Long:    "Subcommands to interact with New Relic APM applications",
 }
 
 var apmDescribeDeployments = &cobra.Command{
@@ -106,6 +118,60 @@ deployment.
 	},
 }
 
+var apmGetApplication = &cobra.Command{
+	Use:   "get",
+	Short: "Get a New Relic application, searching by name or GUID",
+	Long: `Get a New Relic application by ID
+
+The get command performs a query for an APM application by ID.
+`,
+	Example: "newrelic apm application get --name <appName>",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		if apmApplicationName == "" && apmApplicationAccount == "" && apmApplicationGUID == "" {
+			log.Fatal("one of --name, --acountId or --guid are required")
+		}
+
+		client.WithClient(func(nrClient *newrelic.NewRelic) {
+
+			var results []*entities.Entity
+			var err error
+
+			if apmApplicationGUID != "" {
+				results, err = nrClient.Entities.GetEntities([]string{apmApplicationGUID})
+				if err != nil {
+					log.Fatal(err)
+				}
+			} else {
+				params := entities.SearchEntitiesParams{
+					Domain: entities.EntityDomainType("APM"),
+					Type:   entities.EntityType("APPLICATION"),
+				}
+
+				if apmApplicationName != "" {
+					params.Name = apmApplicationName
+				}
+
+				if apmApplicationAccountID != "" {
+					params.Tags = &entities.TagValue{Key: "accountId", Value: apmApplicationAccountID}
+				}
+
+				results, err = nrClient.Entities.SearchEntities(params)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+
+			json, err := prettyjson.Marshal(results)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Println(string(json))
+		})
+	},
+}
+
 func init() {
 	var err error
 
@@ -141,4 +207,12 @@ func init() {
 	if err != nil {
 		log.Error(err)
 	}
+
+	Command.AddCommand(apmApplication)
+	apmApplication.AddCommand(apmGetApplication)
+	apmGetApplication.Flags().IntVarP(&apmApplicationID, "applicationID", "a", 0, "search for results matching the given APM application ID")
+	apmGetApplication.Flags().StringVarP(&apmApplicationName, "name", "n", "", "search for results matching the given APM application name")
+	apmGetApplication.Flags().StringVarP(&apmApplicationGUID, "guid", "g", "", "search for results matching the given APM application GUID")
+	apmGetApplication.Flags().StringVarP(&apmApplicationAccountID, "accountId", "", "", "search for results matching the given APM application account ID")
+
 }
--- internal/apm/command_test.go
+++ internal/apm/command_test.go
@@ -68,3 +68,37 @@ func TestApmDeleteDeployment(t *testing.T) {
 		assert.Equal(t, []string{"true"}, x.Annotations[cobra.BashCompOneRequiredFlag])
 	}
 }
+
+func TestApmApplication(t *testing.T) {
+	command := apmApplication
+	assert.Equal(t, "application", command.Name())
+
+	requiredFlags := []string{}
+
+	for _, r := range requiredFlags {
+		x := command.Flag(r)
+		if x == nil {
+			t.Errorf("Missing required flag: %s\n", r)
+			continue
+		}
+
+		assert.Equal(t, []string{"true"}, x.Annotations[cobra.BashCompOneRequiredFlag])
+	}
+}
+
+func TestApmGetApplication(t *testing.T) {
+	command := apmGetApplication
+	assert.Equal(t, "get", command.Name())
+
+	requiredFlags := []string{}
+
+	for _, r := range requiredFlags {
+		x := command.Flag(r)
+		if x == nil {
+			t.Errorf("Missing required flag: %s\n", r)
+			continue
+		}
+
+		assert.Equal(t, []string{"true"}, x.Annotations[cobra.BashCompOneRequiredFlag])
+	}
+}
