27f43c24aad127bfb0302ac3169e2f16395156bc
Jonathan Thurman <jthurman@newrelic.com>
2020-03-03 11:41:08 -0800

    refactor(docker): Change image name to newrelic/cli

--- build/docker.mk
+++ build/docker.mk
@@ -1,23 +1,23 @@
 #
 # Makefile fragment for Docker actions
 #
-
-IMAGE_NAME     ?= newrelic/$(PROJECT_NAME)
-DOCKER         ?= docker
+DOCKER_ORG        ?= newrelic
+DOCKER_IMAGE_NAME ?= cli
+DOCKER            ?= docker
 
 docker-run: docker-image
 	@echo "=== $(PROJECT_NAME) === [ docker-run       ]: running container:"
-	$(DOCKER) run -it $(IMAGE_NAME):$(PROJECT_VER)
+	$(DOCKER) run -it $(DOCKER_ORG)/$(DOCKER_IMAGE_NAME):$(PROJECT_VER)
 
 docker-image: compile-linux
 	@echo "=== $(PROJECT_NAME) === [ docker-image     ]: building docker image:"
-	$(DOCKER) build -t $(IMAGE_NAME):$(PROJECT_VER) .
+	$(DOCKER) build -t $(DOCKER_ORG)/$(DOCKER_IMAGE_NAME):$(PROJECT_VER) .
 
 docker-clean: docker-rm docker-rmi
 
 docker-rm:
 	@echo "=== $(PROJECT_NAME) === [ docker-clean     ]: removing docker containers:"
-	@for i in "$$($(DOCKER) ps -a | grep $(IMAGE_NAME) | cut -d' ' -f 1)"; do \
+	@for i in "$$($(DOCKER) ps -a | grep "$(DOCKER_ORG)/$(DOCKER_IMAGE_NAME)" | cut -d' ' -f 1)"; do \
 		if [ ! -z "$$i" ]; then \
 			echo -n "=== $(PROJECT_NAME) === [ docker-clean     ]:     "; \
 			$(DOCKER) rm -f $$i ; \
@@ -27,7 +27,7 @@ docker-rm:
 # Must cleanup containers first
 docker-rmi: docker-rm
 	@echo "=== $(PROJECT_NAME) === [ docker-clean     ]: removing docker images:"
-	@for i in "$$($(DOCKER) images | grep $(IMAGE_NAME) | tr -s ' ' | cut -d' ' -f 3)"; do \
+	@for i in "$$($(DOCKER) images | grep "$(DOCKER_ORG)/$(DOCKER_IMAGE_NAME)" | tr -s ' ' | cut -d' ' -f 3)"; do \
 		if [ ! -z "$$i" ]; then \
 			echo "=== $(PROJECT_NAME) === [ docker-clean     ]:     $$i"; \
 			$(DOCKER) rmi -f $$i ; \
