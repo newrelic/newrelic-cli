0332d8cc862f29348c55bf931b8c4401081d0864
Jonathan Thurman <jthurman@newrelic.com>
2020-03-03 22:37:12 -0800

    chore(build): Configure release process for goreleaser

--- build/release.mk
+++ build/release.mk
@@ -5,11 +5,17 @@ GOTOOLS += github.com/goreleaser/goreleaser
 REL_CMD ?= goreleaser
 DIST_DIR ?= ./dist
 
-release-clean:
-	@echo "=== $(PROJECT_NAME) === [ release-clean    ]: distribution files..."
-	@rm -rfv $(DIST_DIR)/*
-
 # Example usage: make release version=0.11.0
 release:
 	@echo "=== $(PROJECT_NAME) === [ release          ]: Generating release."
 	$(RELEASE_SCRIPT) $(version)
+
+release-clean:
+	@echo "=== $(PROJECT_NAME) === [ release-clean    ]: distribution files..."
+	@rm -rfv $(DIST_DIR)/*
+
+release-publish: clean tools
+	@echo "=== $(PROJECT_NAME) === [ release-publish  ]: Publishing release via $(REL_CMD)"
+	$(REL_CMD)
+
+.PHONY: release release-clean release-publish
