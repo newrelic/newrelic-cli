3112179de9820c667898facfcb942c0f7b1e7f60
Sander Blue <sblue@newrelic.com>
2020-03-04 14:26:46 -0800

    tests(nerdgraph): add preliminary test cases

---
+++ internal/nerdgraph/command_test.go
@@ -0,0 +1,26 @@
+package nerdgraph
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNerdGraphCommand(t *testing.T) {
+	assert.NotEmptyf(t, Command.Use, "Need to set Command.%s on Command %s", "Use", Command.CalledAs())
+	assert.NotEmptyf(t, Command.Short, "Need to set Command.%s on Command %s", "Short", Command.CalledAs())
+
+	for _, c := range Command.Commands() {
+		assert.NotEmptyf(t, c.Use, "Need to set Command.%s on Command %s", "Use", c.CommandPath())
+		assert.NotEmptyf(t, c.Short, "Need to set Command.%s on Command %s", "Short", c.CommandPath())
+		assert.NotEmptyf(t, c.Long, "Need to set Command.%s on Command %s", "Long", c.CommandPath())
+		assert.NotEmptyf(t, c.Example, "Need to set Command.%s on Command %s", "Example", c.CommandPath())
+	}
+}
+
+func TestQuery(t *testing.T) {
+	command := queryCmd
+
+	assert.Equal(t, "query", command.Name())
+	assert.True(t, command.HasFlags())
+}
