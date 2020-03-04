cfdf83808ea9a66ae844d69ebc59207aa0239076
Sander Blue <sblue@newrelic.com>
2020-03-04 15:36:45 -0800

    chore(entities): remove 2nd argument from search method now that we dont need it

--- internal/entities/command.go
+++ internal/entities/command.go
@@ -83,7 +83,7 @@ more information.
 				params.Reporting = &reporting
 			}
 
-			entities, err := nrClient.Entities.SearchEntities(params, nil)
+			entities, err := nrClient.Entities.SearchEntities(params)
 			if err != nil {
 				log.Fatal(err)
 			}
