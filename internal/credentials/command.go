f1aee0bc8f4e472c71aea6e33e2a6b9280ac67e1
Jonathan Thurman <jthurman@newrelic.com>
2020-03-06 21:26:30 -0800

    refactor(credentials): Rename vars and improve tests

--- internal/credentials/command.go
+++ internal/credentials/command.go
@@ -20,7 +20,7 @@ var Command = &cobra.Command{
 	Short: "Manage the credential profiles for this tool",
 }
 
-var credentialsAdd = &cobra.Command{
+var cmdAdd = &cobra.Command{
 	Use:   "add",
 	Short: "Add a new credential profile",
 	Long: `Add a new credential profile
@@ -51,7 +51,7 @@ The add command creates a new credential profile for use with the New Relic CLI.
 	},
 }
 
-var credentialsDefault = &cobra.Command{
+var cmdDefault = &cobra.Command{
 	Use:   "default",
 	Short: "Set the default credential profile name",
 	Long: `Set the default credential profile name
@@ -69,7 +69,7 @@ The default command sets the profile to use by default using the specified name.
 	},
 }
 
-var credentialsList = &cobra.Command{
+var cmdList = &cobra.Command{
 	Use:   "list",
 	Short: "List the credential profiles available",
 	Long: `List the credential profiles available
@@ -91,7 +91,7 @@ The list command prints out the available profiles' credentials.
 	},
 }
 
-var credentialsRemove = &cobra.Command{
+var cmdRemove = &cobra.Command{
 	Use:   "remove",
 	Short: "Remove a credential profile",
 	Long: `Remove a credential profiles
@@ -116,41 +116,41 @@ func init() {
 	var err error
 
 	// Add
-	Command.AddCommand(credentialsAdd)
-	credentialsAdd.Flags().StringVarP(&profileName, "profileName", "n", "", "the profile name to add")
-	credentialsAdd.Flags().StringVarP(&region, "region", "r", "", "the US or EU region")
-	credentialsAdd.Flags().StringVarP(&apiKey, "apiKey", "", "", "your personal API key")
-	err = credentialsAdd.MarkFlagRequired("profileName")
+	Command.AddCommand(cmdAdd)
+	cmdAdd.Flags().StringVarP(&profileName, "profileName", "n", "", "the profile name to add")
+	cmdAdd.Flags().StringVarP(&region, "region", "r", "", "the US or EU region")
+	cmdAdd.Flags().StringVarP(&apiKey, "apiKey", "", "", "your personal API key")
+	err = cmdAdd.MarkFlagRequired("profileName")
 	if err != nil {
 		log.Error(err)
 	}
 
-	err = credentialsAdd.MarkFlagRequired("region")
+	err = cmdAdd.MarkFlagRequired("region")
 	if err != nil {
 		log.Error(err)
 	}
 
-	err = credentialsAdd.MarkFlagRequired("apiKey")
+	err = cmdAdd.MarkFlagRequired("apiKey")
 	if err != nil {
 		log.Error(err)
 	}
 
 	// Default
-	Command.AddCommand(credentialsDefault)
-	credentialsDefault.Flags().StringVarP(&profileName, "profileName", "n", "", "the profile name to set as default")
-	err = credentialsDefault.MarkFlagRequired("profileName")
+	Command.AddCommand(cmdDefault)
+	cmdDefault.Flags().StringVarP(&profileName, "profileName", "n", "", "the profile name to set as default")
+	err = cmdDefault.MarkFlagRequired("profileName")
 	if err != nil {
 		log.Error(err)
 	}
 
 	// List
-	Command.AddCommand(credentialsList)
-	credentialsList.Flags().BoolVarP(&showKeys, "show-keys", "s", false, "list the profiles on your keychain")
+	Command.AddCommand(cmdList)
+	cmdList.Flags().BoolVarP(&showKeys, "show-keys", "s", false, "list the profiles on your keychain")
 
 	// Remove
-	Command.AddCommand(credentialsRemove)
-	credentialsRemove.Flags().StringVarP(&profileName, "profileName", "n", "", "the profile name to remove")
-	err = credentialsRemove.MarkFlagRequired("profileName")
+	Command.AddCommand(cmdRemove)
+	cmdRemove.Flags().StringVarP(&profileName, "profileName", "n", "", "the profile name to remove")
+	err = cmdRemove.MarkFlagRequired("profileName")
 	if err != nil {
 		log.Error(err)
 	}
