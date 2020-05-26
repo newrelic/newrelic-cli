d70580100aa9ef1c00d2413c0deced1ff7fa8a84
Sander Blue <sblue@newrelic.com>
2020-05-26 10:46:58 -0700

    chore(chocolatey): remove unnecessary args in Chocolatey ps1 install script (per choco reviewer comments)

--- build/package/chocolatey/tools/chocolateyinstall.ps1
+++ build/package/chocolatey/tools/chocolateyinstall.ps1
@@ -6,19 +6,10 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'msi' #only one of these: exe, msi, msu
-  url           = '' # download url, HTTPS preferred
-  url64bit      = '' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
   file          = $fileLocation
-
   softwareName  = 'newrelic-cli' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-
-  checksum      = ''
-  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = ''
-  checksumType64= 'sha256' #default is checksumType
-
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes= @(0, 3010, 1641)
 }
 
-Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
+Install-ChocolateyInstallPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
