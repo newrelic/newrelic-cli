d9356f6d89084b1d85adfc6b89a517fcee371265
Sander Blue <sblue@newrelic.com>
2020-09-14 17:58:43 -0500

    feat(nerdgraph): implement tutone-generated mutation command alertsPolicyCreate

---
+++ internal/nerdgraph/command_mutation.go
@@ -0,0 +1,51 @@
+package nerdgraph
+
+import (
+	"encoding/json"
+	"github.com/newrelic/newrelic-cli/internal/client"
+	"github.com/newrelic/newrelic-cli/internal/output"
+	"github.com/newrelic/newrelic-cli/internal/utils"
+	"github.com/newrelic/newrelic-client-go/newrelic"
+	"github.com/newrelic/newrelic-client-go/pkg/alerts"
+	"github.com/spf13/cobra"
+)
+
+var cmdMutation = &cobra.Command{
+	Use:     "mutation",
+	Short:   "here is a short description",
+	Long:    "here is a longer description with more detail",
+	Example: "newrelic nerdgraph mutation --help",
+}
+
+var accountID int
+var alertsPolicyCreateInput string
+
+var cmdAlertsPolicyCreate = &cobra.Command{
+	Use:     "alertsPolicyCreate",
+	Short:   "here is a short description",
+	Long:    "here is a longer description with more detail",
+	Example: "newrelic nerdgraph mutation alertsPolicyCreate --input='{\"name\": \"foo\",\"incidentPreference\": \"PER_CONDITION\"}' --accountId=$NEW_RELIC_ACCOUNT_ID\n",
+	Run: func(cmd *cobra.Command, args []string) {
+		client.WithClient(func(nrClient *newrelic.NewRelic) {
+			var input alerts.AlertsPolicyInput
+			err := json.Unmarshal([]byte(alertsPolicyCreateInput), &input)
+			utils.LogIfFatal(err)
+
+			resp, err := nrClient.Alerts.CreatePolicyMutation(accountID, input)
+			utils.LogIfFatal(err)
+
+			utils.LogIfFatal(output.Print(resp))
+		})
+	},
+}
+
+func init() {
+	Command.AddCommand(cmdMutation)
+
+	cmdMutation.AddCommand(cmdAlertsPolicyCreate)
+
+	cmdAlertsPolicyCreate.Flags().IntVar(&accountID, "accountId", 0, "describe the flag here")
+	utils.LogIfError(cmdAlertsPolicyCreate.MarkFlagRequired("accountId"))
+
+	cmdAlertsPolicyCreate.Flags().StringVar(&alertsPolicyCreateInput, "input", "", "describe the flag here")
+}
