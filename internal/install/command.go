ba1c97d872049d1b31cf750298dee21656d28a93
Chris Trombley <ctrombley@newrelic.com>
2020-10-22 10:56:32 -0700

    chore(install): bootstrap install command

---
+++ internal/install/command.go
@@ -0,0 +1,22 @@
+package install
+
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+
+	"github.com/newrelic/newrelic-cli/internal/client"
+	"github.com/newrelic/newrelic-cli/internal/credentials"
+	"github.com/newrelic/newrelic-client-go/newrelic"
+)
+
+// Command represents the install command.
+var Command = &cobra.Command{
+	Use:   "install",
+	Short: "Install New Relic.",
+	Run: func(cmd *cobra.Command, args []string) {
+		// This assumes a default profile exists
+		client.WithClientAndProfile(func(nrClient *newrelic.NewRelic, profile *credentials.Profile) {
+			log.Info("success")
+		})
+	},
+}
---
+++ internal/install/install_test.go
@@ -0,0 +1,13 @@
+// +build unit
+
+package install
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInstall(t *testing.T) {
+	assert.True(t, true)
+}
