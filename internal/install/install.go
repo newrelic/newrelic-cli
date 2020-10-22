2e6766aa8f2bce101710b21454f3802f562b4d81
Chris Trombley <ctrombley@newrelic.com>
2020-10-22 15:21:28 -0700

    chore(install): refactor

--- internal/install/install.go
+++ internal/install/install.go
@@ -1,127 +1,21 @@
 package install
 
-import (
-	"io/ioutil"
-
-	"github.com/go-task/task/v3/taskfile"
-	log "github.com/sirupsen/logrus"
-	"gopkg.in/yaml.v2"
-)
-
 func install() error {
-	// discovery
-	d := getDiscoverer()
+	// Execute the discovery process.
+	d := new(mockDiscoverer)
 	manifest, err := d.discover()
 	if err != nil {
 		return err
 	}
 
-	// retrieve the recipes
-	f := getRecipeFetcher()
+	// Retrieve the relevant recipes.
+	f := new(yamlRecipeFetcher)
 	recipes, err := f.fetch(manifest)
 	if err != nil {
 		return err
 	}
 
-	log.Infof("recipes: %+v", recipes)
-
-	// unmarshal the recipe
-
-	// execute the recipe install steps
+	// Execute the recipe steps.
 
 	return nil
-}
-
-type discoveryManifest struct {
-	processes []process
-	platform  string
-	arch      string
-}
-
-type process struct {
-	name string
-}
-
-func getDiscoverer() discoverer {
-	return new(mockDiscoverer)
-}
-
-func getRecipeFetcher() fetcher {
-	return new(mockRecipeFetcher)
-}
-
-type discoverer interface {
-	discover() (*discoveryManifest, error)
-}
-
-type fetcher interface {
-	fetch(*discoveryManifest) ([]recipe, error)
-}
-
-type mockDiscoverer struct{}
-
-func (m *mockDiscoverer) discover() (*discoveryManifest, error) {
-	x := discoveryManifest{
-		processes: []process{
-			process{
-				name: "java",
-			},
-		},
-		platform: "linux",
-		arch:     "amd64",
-	}
-
-	return &x, nil
-}
-
-type mockRecipeFetcher struct{}
-
-func (m *mockRecipeFetcher) fetch(manifest *discoveryManifest) ([]recipe, error) {
-	var x recipe
-
-	fileName := "internal/install/config.yaml"
-	data, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		return nil, err
-	}
-
-	log.Warnf("data: %+v", string(data))
-
-	err = yaml.Unmarshal(data, &x)
-	if err != nil {
-		return nil, err
-	}
-
-	return []recipe{x}, nil
-}
-
-type recipe struct {
-	Name              string            `yaml:"name"`
-	Description       string            `yaml:"description"`
-	Repository        string            `yaml:"repository"`
-	Platform          string            `yaml:"platform"`
-	Arch              string            `yaml:"arch"`
-	TargetEnvironment string            `yaml:"target_environment"`
-	ProcessMatch      []string          `yaml:"process_match"`
-	MeltMatch         MELTMatch         `yaml:"melt_match"`
-	Install           taskfile.Taskfile `yaml:"install"`
-}
-
-type MELTMatch struct {
-	Events  []CommonMatcher  `yaml:"events"`
-	Metrics []CommonMatcher  `yaml:"metrics"`
-	Logging []LoggingMatcher `yaml:"logging"`
-}
-
-type CommonMatcher struct {
-	Pattern string `yaml:"pattern"`
-}
-
-type LoggingMatcher struct {
-	CommonMatcher
-	Files []string `yaml:"files"`
-}
-
-type recipeFetcher interface {
-	fetch() (error, []*recipe)
 }
---
+++ internal/install/recipes.go
@@ -0,0 +1,58 @@
+package install
+
+import (
+	"io/ioutil"
+
+	"github.com/go-task/task/v3/taskfile"
+	"gopkg.in/yaml.v2"
+)
+
+type recipeFetcher interface {
+	fetch(*discoveryManifest) ([]recipe, error)
+}
+
+type recipe struct {
+	Name              string            `yaml:"name"`
+	Description       string            `yaml:"description"`
+	Repository        string            `yaml:"repository"`
+	Platform          string            `yaml:"platform"`
+	Arch              string            `yaml:"arch"`
+	TargetEnvironment string            `yaml:"target_environment"`
+	ProcessMatch      []string          `yaml:"process_match"`
+	MELTMatch         meltMatch         `yaml:"melt_match"`
+	Install           taskfile.Taskfile `yaml:"install"`
+}
+
+type meltMatch struct {
+	Events  []patternMatcher  `yaml:"events"`
+	Metrics []patternMatcher  `yaml:"metrics"`
+	Logging []loggingMatcher `yaml:"logging"`
+}
+
+type patternMatcher struct {
+	Pattern string `yaml:"pattern"`
+}
+
+type loggingMatcher struct {
+	patternMatcher
+	Files []string `yaml:"files"`
+}
+
+type yamlRecipeFetcher struct{}
+
+func (m *yamlRecipeFetcher) fetch(manifest *discoveryManifest) ([]recipe, error) {
+	var x recipe
+
+	fileName := "internal/install/config.yaml"
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	err = yaml.Unmarshal(data, &x)
+	if err != nil {
+		return nil, err
+	}
+
+	return []recipe{x}, nil
+}
