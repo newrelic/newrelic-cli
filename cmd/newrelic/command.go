e22e0d199d3fff4decb99da0d9574bef53823e27
Chris Trombley <ctrombley@newrelic.com>
2020-10-27 13:43:23 -0700

    feat(profiles): create a profile automatically if it's possible

--- cmd/newrelic/command.go
+++ cmd/newrelic/command.go
@@ -3,8 +3,11 @@ package main
 import (
 	"os"
 
+	"github.com/jedib0t/go-pretty/v6/text"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
+	"github.com/newrelic/newrelic-cli/internal/credentials"
 	"github.com/newrelic/newrelic-cli/internal/output"
 	"github.com/newrelic/newrelic-cli/internal/utils"
 )
@@ -14,11 +17,45 @@ var outputPlain bool
 
 // Command represents the base command when called without any subcommands
 var Command = &cobra.Command{
+	PersistentPreRun:  initializeCLI,
 	Use:               appName,
 	Short:             "The New Relic CLI",
 	Long:              `The New Relic CLI enables users to perform tasks against the New Relic APIs`,
 	Version:           version,
 	DisableAutoGenTag: true, // Do not print generation date on documentation
+}
+
+func initializeCLI(cmd *cobra.Command, args []string) {
+	credentials.WithCredentials(func (c *credentials.Credentials) {
+		if c.DefaultProfile != "" {
+			log.Infof("default profile already exists")
+			return
+		}
+
+		envAPIKey := os.Getenv("NEW_RELIC_API_KEY")
+		envRegion := os.Getenv("NEW_RELIC_REGION")
+
+		if envAPIKey != "" && envRegion != "" {
+			profileName := "default"
+
+			// TODO: DRY this up (exists as well in credentials/command.go)
+			err := c.AddProfile(profileName, envRegion, envAPIKey, "")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			log.Infof("profile %s added", text.FgCyan.Sprint(profileName))
+
+			if len(c.Profiles) == 1 {
+				err := c.SetDefaultProfile(profileName)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				log.Infof("setting %s as default profile", text.FgCyan.Sprint(profileName))
+			}
+		}
+	})
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
--- cmd/newrelic/command_test.go
+++ cmd/newrelic/command_test.go
@@ -3,6 +3,8 @@
 package main
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,4 +13,23 @@ import (
 func TestRootCommand(t *testing.T) {
 	assert.NotEmptyf(t, Command.Use, "Need to set Command.%s on Command %s", "Use", Command.CalledAs())
 	assert.NotEmptyf(t, Command.Short, "Need to set Command.%s on Command %s", "Short", Command.CalledAs())
+}
+
+func TestInitialization(t *testing.T) {
+	f, err := ioutil.TempDir("/tmp", "newrelic")
+	assert.NoError(t, err)
+	defer os.RemoveAll(f)
+
+	// // Initialize the new configuration directory
+	// c, err := LoadCredentials(f)
+	// assert.NoError(t, err)
+	// assert.Equal(t, len(c.Profiles), 0)
+	// assert.Equal(t, c.DefaultProfile, "")
+	// assert.Equal(t, c.ConfigDirectory, f)
+
+	// // Adding a profile with the same name should result in an error
+	// err = c.AddProfile("testCase1", "us", "foot", "")
+	// assert.Error(t, err)
+	// assert.Equal(t, len(c.Profiles), 1)
+	// t st -sassert.True(t, c.profileExists("testCase1"))
 }
