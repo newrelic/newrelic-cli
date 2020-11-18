1ce690cd9baa2496a76f81f36709263b3ec51535
Sander Blue <sblue@newrelic.com>
2020-11-18 15:53:46 -0600

    fix(install): use NEW_RELIC_CLI_VERSION to avoid naming conflict with other OS env var (such as openSUSE)

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@
 #
 # Dependencies: curl, cut, tar, gzip
 #
-# The version to install and the binary location can be passed in via VERSION and DESTDIR respectively.
+# The version to install and the binary location can be passed in via NEW_RELIC_CLI_VERSION and DESTDIR respectively.
 #
 
 set -o errexit
@@ -30,15 +30,18 @@ done
 LATEST_URL="https://github.com/newrelic/newrelic-cli/releases/latest"
 DESTDIR="${DESTDIR:-/usr/local/bin}"
 
-if [ -z "$VERSION" ]; then
-	VERSION=$(curl -sLI -o /dev/null -w '%{url_effective}' $LATEST_URL | cut -d "v" -f 2)
+if [ -z "$NEW_RELIC_CLI_VERSION" ]; then
+	echo "CHECK PASSED v${NEW_RELIC_CLI_VERSION}"
+
+	NEW_RELIC_CLI_VERSION=$(curl -sLI -o /dev/null -w '%{url_effective}' $LATEST_URL | cut -d "v" -f 2)
 fi
 
-echo "Installing New Relic CLI v${VERSION}"
+echo "Installing New Relic CLI v${NEW_RELIC_CLI_VERSION} - $1"
 
 # Run the script in a temporary directory that we know is empty.
 SCRATCH=$(mktemp -d || mktemp -d -t 'tmp')
 cd "$SCRATCH"
+
 
 function error {
   echo "An error occurred installing the tool."
@@ -47,7 +50,7 @@ function error {
 
 trap error ERR
 
-RELEASE_URL="https://github.com/newrelic/newrelic-cli/releases/download/v${VERSION}/newrelic-cli_${VERSION}_${OS}_x86_64.tar.gz"
+RELEASE_URL="https://github.com/newrelic/newrelic-cli/releases/download/v${NEW_RELIC_CLI_VERSION}/newrelic-cli_${NEW_RELIC_CLI_VERSION}_${OS}_x86_64.tar.gz"
 
 # Download & unpack the release tarball.
 curl -sL --retry 3 "${RELEASE_URL}" | tar -xz
