907e38f25ff92acc33a488150b5943b0528cc126
Chris Trombley <ctrombley@newrelic.com>
2020-12-16 13:25:46 -0800

    tests(install): fill out unit test coverage for nerdstorageExecutionStatusReporter

--- internal/install/mock_nerdstorage_client.go
+++ internal/install/mock_nerdstorage_client.go
@@ -4,22 +4,28 @@ import "github.com/newrelic/newrelic-client-go/pkg/nerdstorage"
 
 // nolint:unused,deadcode
 type mockNerdstorageClient struct {
-	respBody interface{}
-	err      error
+	respBody                              interface{}
+	userScopeError                        error
+	entityScopeError                      error
+	writeDocumentWithUserScopeCallCount   int
+	writeDocumentWithEntityScopeCallCount int
 }
 
 // nolint:unused,deadcode
 func newMockNerdstorageClient() *mockNerdstorageClient {
 	return &mockNerdstorageClient{
-		respBody: struct{}{},
-		err:      nil,
+		respBody:         struct{}{},
+		userScopeError:   nil,
+		entityScopeError: nil,
 	}
 }
 
 func (c *mockNerdstorageClient) WriteDocumentWithUserScope(nerdstorage.WriteDocumentInput) (interface{}, error) {
-	return c.respBody, c.err
+	c.writeDocumentWithUserScopeCallCount++
+	return c.respBody, c.userScopeError
 }
 
 func (c *mockNerdstorageClient) WriteDocumentWithEntityScope(string, nerdstorage.WriteDocumentInput) (interface{}, error) {
-	return c.respBody, c.err
+	c.writeDocumentWithEntityScopeCallCount++
+	return c.respBody, c.entityScopeError
 }
