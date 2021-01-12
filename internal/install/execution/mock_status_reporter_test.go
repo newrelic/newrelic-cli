93eb44e2f2d34e4bffbd68dd6f5c3e610f936f10
Zach Leslie <zleslie@newrelic.com>
2021-01-12 15:26:08 -0800

    tests(install): include test for mock interface compliance

---
+++ internal/install/execution/mock_status_reporter_test.go
@@ -0,0 +1,14 @@
+// +build unit
+
+package execution
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMockStatusReporter_interface(t *testing.T) {
+	var r StatusReporter = NewMockStatusReporter()
+	require.NotNil(t, r)
+}
