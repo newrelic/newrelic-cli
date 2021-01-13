b53a0f8451fc32cfaa13bbd690f7a33cf1d54e42
Zach Leslie <zleslie@newrelic.com>
2021-01-13 15:11:30 -0800

    refactor(install): invert use of status reporter and status rollup

--- internal/install/execution/status_reporter.go
+++ internal/install/execution/status_reporter.go
@@ -4,12 +4,13 @@ import "github.com/newrelic/newrelic-cli/internal/install/types"
 
 // StatusReporter is responsible for reporting the status of recipe execution.
 type StatusReporter interface {
-	ReportRecipeFailed(event RecipeStatusEvent) error
-	ReportRecipeInstalled(event RecipeStatusEvent) error
-	ReportRecipeSkipped(event RecipeStatusEvent) error
-	ReportRecipesAvailable(recipes []types.Recipe) error
-	ReportRecipeAvailable(recipe types.Recipe) error
-	ReportComplete() error
+	ReportComplete(status *StatusRollup) error
+	ReportRecipeAvailable(status *StatusRollup, recipe types.Recipe) error
+	ReportRecipeFailed(status *StatusRollup, event RecipeStatusEvent) error
+	ReportRecipeInstalled(status *StatusRollup, event RecipeStatusEvent) error
+	ReportRecipeInstalling(status *StatusRollup, event RecipeStatusEvent) error
+	ReportRecipeSkipped(status *StatusRollup, event RecipeStatusEvent) error
+	ReportRecipesAvailable(status *StatusRollup, recipes []types.Recipe) error
 }
 
 // RecipeStatusEvent represents an event in a recipe's execution.
