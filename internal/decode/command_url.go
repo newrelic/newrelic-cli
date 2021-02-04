792cc110e373a37178efa38c91150d78903e221e
nfoley <nfoley@newrelic.com>
2021-02-04 21:59:12 +0000

    feat(decode): NR CLI Translate base64 encoded urls command

--- internal/decode/command_url.go
+++ internal/decode/command_url.go
@@ -14,15 +14,15 @@ import (
 )
 
 var (
-	paramElement string
-	jsonElement  string
+	paramElement  string
+	searchElement string
 )
 
 // Command represents the decode command.
 var cmdDecode = &cobra.Command{
 	Use:     "url",
 	Short:   "Decodes NR1 URL Strings ",
-	Example: `newrelic decode url -p="pane" -j="entityId" https://one.newrelic.com/launcher/nr1-core.home?pane=eyJuZXJkbGV0SWQiOiJucjEtY29yZS5ob21lLXNjcmVlbiJ9&platform[accountId]=1`,
+	Example: `newrelic decode url -p="pane" -s="entityId" https://one.newrelic.com/launcher/nr1-core.home?pane=eyJuZXJkbGV0SWQiOiJucjEtY29yZS5ob21lLXNjcmVlbiJ9&platform[accountId]=1`,
 
 	Run: func(cmd *cobra.Command, args []string) {
 		relicString := strings.Join(args, "")
@@ -30,45 +30,45 @@ var cmdDecode = &cobra.Command{
 		relicString = u.Query().Get(paramElement)
 		decoded, err := base64.StdEncoding.DecodeString(relicString)
 
-		if err != nil {
-			fmt.Println("decode error:", err)
-		}
+		utils.LogIfFatal(err)
 
 		decodedURL := string(decoded)
-		fmt.Println(decodedURL)
-		entityID := strings.Contains(decodedURL, jsonElement)
+		entityID := strings.Contains(decodedURL, searchElement)
 
-		if entityID {
-			entityIDString := gjson.Get(decodedURL, jsonElement)
-			decodedURL = entityIDString.String()
-
-			var regex = regexp.MustCompile(`[a-zA-Z0-9\+]*={0,3}`)
-			//regex is used to determine correct base64 length
-			if len(regex.FindStringIndex(decodedURL)) > 0 {
-				for (len(decodedURL) % 4) != 0 {
-					decodedURL += "="
-				}
-			}
-			entityVal := string(decoded)
-
-			decoded, err := base64.StdEncoding.DecodeString(decodedURL)
-			if err != nil {
-				//fmt.Println("decode error:", err)
-				entityIDString := gjson.Get(entityVal, jsonElement)
-				fmt.Println(entityIDString)
-			}
-
-			decodedURL := string(decoded)
-			fmt.Println(decodedURL)
+		if !entityID {
+			fmt.Printf("%s not found in %s", searchElement, decodedURL)
+			return
 		}
+
+		entityIDString := gjson.Get(decodedURL, searchElement)
+		decodedEntityID := entityIDString.String()
+
+		var regex = regexp.MustCompile(`[a-zA-Z0-9\+]*={0,3}`)
+		//regex is used to determine correct base64 length
+		if len(regex.FindStringIndex(decodedEntityID)) > 0 {
+			for (len(decodedEntityID) % 4) != 0 {
+				decodedEntityID += "="
+			}
+		}
+		entityVal := string(decoded)
+
+		decoded, err = base64.StdEncoding.DecodeString(decodedEntityID)
+		if err != nil {
+			entityIDString := gjson.Get(entityVal, searchElement)
+			fmt.Println(entityIDString)
+		}
+
+		decodedEntityID = string(decoded)
+		fmt.Println(decodedEntityID)
+
 	},
 }
 
 func init() {
 	Command.AddCommand(cmdDecode)
-	cmdDecode.Flags().StringVarP(&paramElement, "param", "p", "", "The Param you want to decode")
+	cmdDecode.Flags().StringVarP(&paramElement, "param", "p", "", "the query parameter you want to decode")
 	utils.LogIfError(cmdDecode.MarkFlagRequired("param"))
 
-	cmdDecode.Flags().StringVarP(&jsonElement, "json", "j", "", "The Json element you want returned")
-	utils.LogIfError(cmdDecode.MarkFlagRequired("json"))
+	cmdDecode.Flags().StringVarP(&searchElement, "search", "s", "", "the search key you want returned")
+	utils.LogIfError(cmdDecode.MarkFlagRequired("search"))
 }
