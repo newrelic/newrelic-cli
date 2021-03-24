60280e3adb4f03bb0f7ac518612cef88af63d974
Zach Leslie <zleslie@newrelic.com>
2021-03-24 11:43:20 -0700

    chore: point the user to a different document

--- internal/install/types/errors.go
+++ internal/install/types/errors.go
@@ -7,4 +7,4 @@ import (
 // ErrInterrupt represents a context cancellation.
 var ErrInterrupt = errors.New("operation canceled")
 
-var ErrorFetchingLicenseKey = errors.New("Oops, we're having some difficulties fetching your license key. Please try again later, or see our documentation for installing manually https://developer.newrelic.com/automate-workflows/get-started-new-relic-cli")
+var ErrorFetchingLicenseKey = errors.New("Oops, we're having some difficulties fetching your license key. Please try again later, or see our documentation for installing manually https://docs.newrelic.com/docs/using-new-relic/cross-product-functions/install-configure/install-new-relic")
