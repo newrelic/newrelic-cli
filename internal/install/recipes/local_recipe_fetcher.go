01caafc98e01d490614a4d1c582841fe7dae65aa
Zach Leslie <zleslie@newrelic.com>
2021-04-19 11:23:57 -0600

    chore(recipes): flesh out the local fetcher FetchRecipes method

--- internal/install/recipes/local_recipe_fetcher.go
+++ internal/install/recipes/local_recipe_fetcher.go
@@ -3,8 +3,11 @@ package recipes
 import (
 	"context"
 	"embed"
+	"path/filepath"
 
+	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
 
 	"github.com/newrelic/newrelic-cli/internal/install/types"
 )
@@ -25,27 +28,49 @@ func (f *LocalRecipeFetcher) FetchRecommendations(ctx context.Context, manifest 
 }
 
 func (f *LocalRecipeFetcher) FetchRecipes(ctx context.Context, manifest *types.DiscoveryManifest) ([]types.Recipe, error) {
-	recurseRecipeFiles()
+	var recipes []types.Recipe
 
-	return nil, nil
+	files := recurseDirectory("recipes/newrelic/infrastructure")
+
+	for _, file := range files {
+		var r types.Recipe
+
+		content, err := localRecipes.ReadFile(file)
+		if err != nil {
+			return nil, errors.Wrap(err, "error reading recipe file")
+		}
+
+		err = yaml.Unmarshal(content, &r)
+		if err != nil {
+			return nil, errors.Wrap(err, "error unmarshaling recipe file into Recipe")
+		}
+
+		recipes = append(recipes, r)
+	}
+
+	log.Debugf("%d embedded recipes found", len(recipes))
+
+	return recipes, nil
 }
 
-func recurseRecipeFiles() {
-
-	log.Warnf("content: %+v", localRecipes)
-
-	dirs := recurseDirectories("recipes/newrelic/infrastructure")
-
-	results, err := localRecipes.ReadDir("recipes/newrelic/infrastructure")
+func recurseDirectory(startDir string) []string {
+	log.Debugf("recursing %s", startDir)
+	var fileNames []string
+	results, err := localRecipes.ReadDir(startDir)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for _, r := range results {
-		log.Warnf("name: %+v\n", r.Name())
+		if r.Type().IsDir() {
+			files := recurseDirectory(filepath.Join(startDir, r.Name()))
+			fileNames = append(fileNames, files...)
+		}
+
+		if r.Type().IsRegular() {
+			fileNames = append(fileNames, filepath.Join(startDir, r.Name()))
+		}
 	}
-}
 
-func recurseDirectories(startDir string) []string {
-
+	return fileNames
 }
--- internal/install/recipes/local_recipe_fetcher_test.go
+++ internal/install/recipes/local_recipe_fetcher_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"testing"
 
+	log "github.com/sirupsen/logrus"
+
 	"github.com/newrelic/newrelic-cli/internal/install/types"
 	"github.com/stretchr/testify/require"
 )
@@ -17,6 +19,7 @@ func TestLocalRecipeFetcher_FetchRecommendations(t *testing.T) {
 }
 
 func TestLocalRecipeFetcher_FetchRecipes(t *testing.T) {
+	log.SetLevel(log.DebugLevel)
 	f := LocalRecipeFetcher{}
 	recipes, err := f.FetchRecipes(context.Background(), &types.DiscoveryManifest{})
 	require.NoError(t, err)
