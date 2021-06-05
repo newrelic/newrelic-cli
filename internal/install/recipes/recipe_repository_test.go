d0e8d8c0bfed72e8ccbaf94927ab8de8de044937
Julien Erard <jerard@newrelic.com>
2021-06-04 19:17:23 -0700

    refactor(install): add more repository tests

--- internal/install/recipes/recipe_repository_test.go
+++ internal/install/recipes/recipe_repository_test.go
@@ -88,6 +88,39 @@ func Test_ShouldFindMostMatchingSingleRecipeWithoutPlatform(t *testing.T) {
 	require.Equal(t, results[0].ID, "id1")
 }
 
+func Test_ShouldDiscardMostMatchingWithoutAllFieldsMatching(t *testing.T) {
+	Setup()
+	givenCachedRecipeOs("id1", "my-recipe2", types.OpenInstallationOperatingSystemTypes.LINUX)
+	givenCachedRecipeOsPlatform("id2", "my-recipe2", types.OpenInstallationOperatingSystemTypes.LINUX, types.OpenInstallationPlatformTypes.DEBIAN)
+	givenCachedRecipeOsPlatform("id3", "my-recipe2", types.OpenInstallationOperatingSystemTypes.LINUX, types.OpenInstallationPlatformTypes.UBUNTU)
+	givenCachedRecipeOsPlatformVersionArch("id4", "my-recipe2", types.OpenInstallationOperatingSystemTypes.WINDOWS, "10.0", "x86_64")
+	discoveryManifest.OS = "linux"
+	discoveryManifest.PlatformVersion = "10.0"
+	discoveryManifest.KernelArch = "x86_64"
+
+	results, _ := repository.FindAll(discoveryManifest)
+
+	require.Len(t, results, 1)
+	require.Equal(t, results[0].ID, "id1")
+}
+
+func Test_ShouldFindMultipleNames(t *testing.T) {
+	Setup()
+	givenCachedRecipeOs("id1", "infra", types.OpenInstallationOperatingSystemTypes.LINUX)
+	givenCachedRecipeOsPlatform("id2", "infra", types.OpenInstallationOperatingSystemTypes.LINUX, types.OpenInstallationPlatformTypes.DEBIAN)
+	givenCachedRecipeOsPlatform("id3", "logs", types.OpenInstallationOperatingSystemTypes.LINUX, types.OpenInstallationPlatformTypes.UBUNTU)
+	givenCachedRecipeOsArch("id4", "logs", types.OpenInstallationOperatingSystemTypes.LINUX, "x86_64")
+	discoveryManifest.OS = "linux"
+	discoveryManifest.Platform = "debian"
+	discoveryManifest.KernelArch = "x86_64"
+
+	results, _ := repository.FindAll(discoveryManifest)
+
+	require.Len(t, results, 2)
+	require.True(t, containsId(results, "id2"))
+	require.True(t, containsId(results, "id4"))
+}
+
 func Test_matchRecipeCriteria_Basic(t *testing.T) {
 	Setup()
 	discoveryManifest.Platform = "linux"
@@ -137,6 +170,29 @@ func givenCachedRecipeOsPlatform(id string, name string, os types.OpenInstallati
 	return r
 }
 
+func givenCachedRecipeOsArch(id string, name string, os types.OpenInstallationOperatingSystem, arch string) *types.OpenInstallationRecipe {
+	r := createRecipe(id, name)
+	t := types.OpenInstallationRecipeInstallTarget{
+		KernelArch: arch,
+		Os:         os,
+	}
+	r.InstallTargets = append(r.InstallTargets, t)
+	recipeCache = append(recipeCache, *r)
+	return r
+}
+
+func givenCachedRecipeOsPlatformVersionArch(id string, name string, os types.OpenInstallationOperatingSystem, platformVersion string, arch string) *types.OpenInstallationRecipe {
+	r := createRecipe(id, name)
+	t := types.OpenInstallationRecipeInstallTarget{
+		KernelArch:      arch,
+		Os:              os,
+		PlatformVersion: platformVersion,
+	}
+	r.InstallTargets = append(r.InstallTargets, t)
+	recipeCache = append(recipeCache, *r)
+	return r
+}
+
 func givenCachedRecipe(id string, name string) *types.OpenInstallationRecipe {
 	r := createRecipe(id, name)
 	recipeCache = append(recipeCache, *r)
@@ -149,4 +205,14 @@ func createRecipe(id string, name string) *types.OpenInstallationRecipe {
 		Name: name,
 	}
 	return r
+}
+
+func containsId(recipes []types.OpenInstallationRecipe, id string) bool {
+	for _, recipe := range recipes {
+		if recipe.ID == id {
+			return true
+		}
+	}
+
+	return false
 }
