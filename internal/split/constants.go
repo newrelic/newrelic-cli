1cc1385c918715d87c4226bc689e64fdf9d9ca1f
Alexander Brunner <abrunner@newrelic.com>
2021-07-19 18:26:58 -0500

    chore: clean up

--- internal/split/constants.go
+++ internal/split/constants.go
@@ -1,10 +1,8 @@
 package split
 
 var (
-	// Define splits and eliminate old ones as needed
-	// The value corresponds to the name of the split in Split.io
-	VirtuosoCLITest  = "virtuoso_cli_test"
-	VirtuosoCliTest2 = "virtuoso_cli_test_2"
-	// Used for unit-testing
-	MockSplits = "feature1 on\nfeature2 off\n"
+// Define splits and remove old ones as needed
+// The value corresponds to the name of the split in Split.io
+
+// VirtuosoCLITest = "virtuoso_cli_test"
 )
--- internal/split/split_service_test.go
+++ internal/split/split_service_test.go
@@ -11,6 +11,9 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// Used for unit-testing
+var mockSplits = "feature1 on\nfeature2 off\n"
+
 func TestNewSplitService(t *testing.T) {
 	service, _ := NewService("localhost")
 
@@ -57,7 +60,7 @@ func createMockSplits() string {
 		log.Errorf("could not get user home directory: %s", err)
 	}
 	// Create a temporary file that holds test splits for testing purposes
-	blob := []byte(MockSplits)
+	blob := []byte(mockSplits)
 	filename := dir + "/mock.split"
 	err = ioutil.WriteFile(filename, blob, 0777)
 	if err != nil {
