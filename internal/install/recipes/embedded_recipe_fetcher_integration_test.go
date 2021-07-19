3c71650c0b54ec2dd4a72530d6099e2b9e93a08a
Chris Trombley <ctrombley@gmail.com>
2021-07-19 09:37:06 -0700

    Revert "Revert "refactor(install): eliminate dependency on open-install-library""

---
+++ internal/install/recipes/embedded_recipe_fetcher_integration_test.go
@@ -0,0 +1,18 @@
+//+build integration
+
+package recipes
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestFetchRecipes(t *testing.T) {
+	f := NewEmbeddedRecipeFetcher()
+
+	r, err := f.FetchRecipes(context.Background())
+	require.NoError(t, err)
+	require.Greater(t, len(r), 0)
+}
