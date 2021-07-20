ada9e2dd5ebc5be886bae7a5e326f70d9dad4eae
Chris Trombley <ctrombley@newrelic.com>
2021-07-20 09:10:36 -0700

    fix(install): suppress spinner output during verbose logging

--- internal/install/ux/spinner.go
+++ internal/install/ux/spinner.go
@@ -5,6 +5,8 @@ import (
 	"time"
 
 	spinnerLib "github.com/briandowns/spinner"
+	"github.com/newrelic/newrelic-cli/internal/config"
+	log "github.com/sirupsen/logrus"
 )
 
 const (
@@ -29,15 +31,25 @@ func NewSpinner() *Spinner {
 }
 
 func (s *Spinner) Start(msg string) {
-	s.Spinner = spinnerLib.New(charSet, interval)
-	s.Prefix = indentation
-	s.Suffix = fmt.Sprintf(" %s", msg)
-	s.Spinner.Start()
+	// Suppress spinner output when logging at debug or trace level.
+	// Output is garbled when verbose log messages are sent during an active spinner.
+	if !config.Logger.IsLevelEnabled(log.DebugLevel) {
+		s.Spinner = spinnerLib.New(charSet, interval)
+		s.Prefix = indentation
+		s.Suffix = fmt.Sprintf(" %s", msg)
+		s.Spinner.Start()
+	}
+	log.Debug(msg)
 }
 
 func (s *Spinner) Stop() {
-	s.Spinner.Stop()
-	fmt.Println(s.Suffix)
+	// Suppress stopping the spinner when logging at debug or trace level.
+	// See above.
+	if !config.Logger.IsLevelEnabled(log.DebugLevel) {
+		s.Spinner.Stop()
+		fmt.Println(s.Suffix)
+	}
+	log.Debug(s.Suffix)
 }
 
 func (s *Spinner) Fail(msg string) {
