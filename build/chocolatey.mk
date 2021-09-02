b58221d786b5db9ba5a15112b406f18adbdf013e
Chris Trombley <ctrombley@newrelic.com>
2021-09-02 10:32:08 -0700

    fix: get chocolatey publish pipeline working

--- build/chocolatey.mk
+++ build/chocolatey.mk
@@ -1,8 +1,8 @@
 #
 # Makefile fragment for Chocolatey actions
 #
-CHOCO         ?= docker run --rm -v $$PWD:$$PWD -w $$PWD linuturk/mono-choco
-CHOCOLATEY_BUILD_DIR	?= build/package/chocolatey
+CHOCOLATEY_BUILD_DIR    ?= build/package/chocolatey
+CHOCO                   ?= docker run --rm -v $$PWD/$(CHOCOLATEY_BUILD_DIR):$$PWD -w $$PWD linuturk/mono-choco
 
 chocolatey-publish: chocolatey-build
 	@echo "=== $(PROJECT_NAME) === [ chocolatey-publish ]: publishing chocolatey package"
@@ -10,19 +10,15 @@ chocolatey-publish: chocolatey-build
 		echo "Failure: CHOCOLATEY_API_KEY not set" ; \
 		exit 1 ; \
 	fi
-	@cd $(CHOCOLATEY_BUILD_DIR) && \
-		$(CHOCO) push --source https://chocolatey.org/ -k ${CHOCOLATEY_API_KEY} newrelic-cli.${PROJECT_VER_TAGGED}.nupkg \
-	; cd -
+	$(CHOCO) push --source https://chocolatey.org/ -k ${CHOCOLATEY_API_KEY} newrelic-cli.${PROJECT_VER_TAGGED}.nupkg
 
 
 chocolatey-build:
 	@echo "=== $(PROJECT_NAME) === [ chocolatey-build ]: publishing chocolatey package"
 	@cp LICENSE $(CHOCOLATEY_BUILD_DIR)/tools/LICENSE.txt
-	@cd $(CHOCOLATEY_BUILD_DIR) && \
-		curl -sL -o tools/NewRelicCLIInstaller.msi https://download.newrelic.com/install/newrelic-cli/v${PROJECT_VER_TAGGED}/NewRelicCLIInstaller.msi && \
-		rm -f newrelic-cli.${PROJECT_VER_TAGGED}.nupkg && \
-		sed -i '' -e "s/    <version>.*<\/version>/    <version>${PROJECT_VER_TAGGED}<\/version>/g" newrelic-cli.nuspec && \
-		$(CHOCO) pack \
-	; cd -
+	@curl -sL -o $(CHOCOLATEY_BUILD_DIR)/tools/NewRelicCLIInstaller.msi https://download.newrelic.com/install/newrelic-cli/v${PROJECT_VER_TAGGED}/NewRelicCLIInstaller.msi
+	@rm -f newrelic-cli.${PROJECT_VER_TAGGED}.nupkg
+	@sed -i '' -e "s/    <version>.*<\/version>/    <version>${PROJECT_VER_TAGGED}<\/version>/g" $(CHOCOLATEY_BUILD_DIR)/newrelic-cli.nuspec
+	$(CHOCO) pack
 
 .PHONY: chocolatey-build chocolatey-publish
