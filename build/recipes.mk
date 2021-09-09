336e3718d220a88e46de2f836308370d145e79e0
Chris Trombley <ctrombley@newrelic.com>
2021-09-09 08:40:20 -0700

    chore(install): incorporate code review feedback

--- build/recipes.mk
+++ build/recipes.mk
@@ -4,7 +4,7 @@
 #
 EMBEDDED_RECIPES_PATH         ?= internal/install/recipes/files
 RECIPE_ARCHIVE_VERSION        ?= $(shell curl -Ls https://s3.us-east-1.amazonaws.com/nr-downloads-main/install/open-install-library/currentVersion.txt)
-RECIPE_ARCHIVE_URL            ?= https://download.newrelic.com/install/open-install-library/${RECIPE_ARCHIVE_VERSION}/recipes.zip
+RECIPE_ARCHIVE_URL            ?= https://s3.us-east-1.amazonaws.com/nr-downloads-main/install/open-install-library/${RECIPE_ARCHIVE_VERSION}/recipes.zip
 
 recipes: recipes-clean recipes-fetch
 
