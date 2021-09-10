e27e92536977a5cb632dfb851761ca6f46a08df9
Chris Trombley <ctrombley@newrelic.com>
2021-09-10 14:55:39 -0700

    chore: appease chocolatey admins

--- build/package/chocolatey/tools/chocolateyinstall.ps1
+++ build/package/chocolatey/tools/chocolateyinstall.ps1
@@ -1,15 +1,15 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'NewRelicCLIInstaller.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'msi' #only one of these: exe, msi, msu
+  fileType      = 'msi'
   file          = $fileLocation
-  softwareName  = 'newrelic-cli' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  softwareName  = 'newrelic-cli'
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes= @(0, 3010, 1641)
 }
 
-Install-ChocolateyInstallPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-install-package
+Install-ChocolateyInstallPackage @packageArgs
