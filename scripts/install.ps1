56122a5775a93966f17a97b8f05e1d58a5620b89
natecanfield822 <ncanfield@newrelic.com>
2021-12-15 09:24:47 -0500

    feat(install): Require powershell to be run as admin
    This adds the requires tag for admin for powershell.

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,3 +1,4 @@
+#Requires -RunAsAdministrator
 [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls';
 $version = (New-Object System.Net.WebClient).DownloadString("https://download.newrelic.com/install/newrelic-cli/currentVersion.txt").Trim();
 (New-Object System.Net.WebClient).DownloadFile("https://download.newrelic.com/install/newrelic-cli/${version}/NewRelicCLIInstaller.msi", "$env:TEMP\NewRelicCLIInstaller.msi");
