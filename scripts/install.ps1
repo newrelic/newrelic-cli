57c2df616cc6a8d34ca5fcf5e2dfc7a48c5c13ba
natecanfield822 <ncanfield@newrelic.com>
2021-12-15 16:05:55 -0500

    fix: Grammer/spelling fix

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,6 +1,6 @@
 $p = New-Object System.Security.Principal.WindowsPrincipal([System.Security.Principal.WindowsIdentity]::GetCurrent())
 if (!$p.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    throw 'This script requires admin privledges to run and the current Windows PowerShell session is not running as Administrator. Start Windows PowerShell by  using the Run as Administrator option, and then try running the script again.'
+    throw 'This script requires admin privileges to run and the current Windows PowerShell session is not running as Administrator. Start Windows PowerShell by using the Run as Administrator option, and then try running the script again.'
 }
 [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls';
 $version = (New-Object System.Net.WebClient).DownloadString("https://download.newrelic.com/install/newrelic-cli/currentVersion.txt").Trim();
