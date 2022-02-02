0cb267bbf81899ab20e6ec10064c41c07f2ab881
Sander Blue <sblue@newrelic.com>
2022-02-02 11:05:32 -0600

    chore(install): remove unnecessary test, add additional assertion to another test

--- internal/install/recipes/script_evaluation_recipe_filterer_test.go
+++ internal/install/recipes/script_evaluation_recipe_filterer_test.go
@@ -77,41 +77,5 @@ func TestScriptEvaluationRecipeFilterer_ShouldFailPreInstallWithDetectedEventCap
 
 	require.Equal(t, 1, mockInstallEventsClient.CreateRecipeEventCallCount)
 	require.Equal(t, 1, mockReporter.RecipeDetectedCallCount)
-}
-
-func TestScriptEvaluationRecipeFilterer_ShouldFailPreInstallWithUnsupportedEventCaptured(t *testing.T) {
-	recipe := types.OpenInstallationRecipe{
-		Name:         "test-recipe",
-		ProcessMatch: []string{"apache2"},
-		PreInstall: types.OpenInstallationPreInstallConfiguration{
-			// Using exit code 1 should trigger the UNSUPPORTED event
-			RequireAtDiscovery: `echo '"{\"aUsefulKey\":\"a useful value\"}"' >&2; exit 1`,
-		},
-	}
-
-	matchedProcess := mockProcess{
-		cmdline: "apache2",
-		name:    `apache2`,
-		pid:     int32(1234),
-	}
-
-	m := types.DiscoveryManifest{
-		DiscoveredProcesses: []types.GenericProcess{matchedProcess},
-	}
-
-	mockInstallEventsClient := execution.NewMockInstallEventsClient()
-	installEventsReporter := execution.NewInstallEventsReporter(mockInstallEventsClient)
-
-	mockReporter := execution.NewMockStatusReporter()
-	statusSubscribers := []execution.StatusSubscriber{installEventsReporter, mockReporter}
-	platformLinkGenerator := execution.NewPlatformLinkGenerator()
-	installStatus := execution.NewInstallStatus(statusSubscribers, platformLinkGenerator)
-
-	r := NewScriptEvaluationRecipeFilterer(installStatus)
-
-	err := r.CheckCompatibility(context.Background(), &recipe, &m)
-	require.Error(t, err)
-
-	require.Equal(t, 1, mockInstallEventsClient.CreateRecipeEventCallCount)
-	require.Equal(t, 1, mockReporter.RecipeUnsupportedCallCount)
+	require.Equal(t, 0, mockReporter.RecipeUnsupportedCallCount)
 }
