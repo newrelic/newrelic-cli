85a9e60a912ef873a68b8f6aad947313a72b89c8
Julien4218 <jerard@newrelic.com>
2022-02-03 15:14:07 -0800

    fix(install): fix test

--- internal/install/bundler_test.go
+++ internal/install/bundler_test.go
@@ -6,46 +6,80 @@ package install
 import (
 	"testing"
 
+	recipes "github.com/newrelic/newrelic-cli/internal/install/recipes"
 	"github.com/newrelic/newrelic-cli/internal/install/types"
 
 	"github.com/stretchr/testify/require"
+	"strings"
 )
 
-func TestCreateBundles_ShouldCreateTwoBundles(t *testing.T) {
-	recipes := []types.OpenInstallationRecipe{
-		{
-			Name: types.InfraAgentRecipeName,
-		},
-		{
-			Name: types.LoggingRecipeName,
-		},
-		{
-			Name: types.GoldenRecipeName,
-		},
-		{
-			Name: "mysql-open-source-integration",
-		},
-	}
-	coreBundle, extrasBundle := createBundles(coreBundleRecipeNames, recipes)
+func TestCreateBundles_ShouldCreateCoreBundle(t *testing.T) {
+	Setup()
+	givenRecipe("id1", types.InfraAgentRecipeName)
+	givenRecipe("id2", types.LoggingRecipeName)
+	givenRecipe("id3", types.GoldenRecipeName)
+	givenRecipe("id4", "mysql")
+
+	bundler := givenBundler()
+	coreBundle := bundler.createCoreBundle()
 
 	require.Equal(t, 3, len(coreBundle))
-	require.Greater(t, 1, len(extrasBundle))
+	require.NotNil(t, findRecipeByName(coreBundle, types.InfraAgentRecipeName))
+	require.NotNil(t, findRecipeByName(coreBundle, types.LoggingRecipeName))
+	require.NotNil(t, findRecipeByName(coreBundle, types.GoldenRecipeName))
+	require.Nil(t, findRecipeByName(coreBundle, "mysql"))
 }
 
-func TestCreateBundles_ShouldCreateEmptyExtrasBundle(t *testing.T) {
-	recipes := []types.OpenInstallationRecipe{
-		{
-			Name: types.InfraAgentRecipeName,
-		},
-		{
-			Name: types.LoggingRecipeName,
-		},
-		{
-			Name: types.GoldenRecipeName,
-		},
-	}
-	coreBundle, extrasBundle := createBundles(coreBundleRecipeNames, recipes)
+func TestCreateBundles_ShouldCreateEmptyCoreBundle(t *testing.T) {
+	Setup()
+	givenRecipe("id4", "mysql")
 
-	require.Equal(t, 3, len(coreBundle))
-	require.Equal(t, 0, len(extrasBundle))
+	bundler := givenBundler()
+	coreBundle := bundler.createCoreBundle()
+
+	require.Equal(t, 0, len(coreBundle))
+}
+
+func findRecipeByName(recipes []types.OpenInstallationRecipe, name string) *types.OpenInstallationRecipe {
+	for _, r := range recipes {
+		if strings.EqualFold(r.Name, name) {
+			return &r
+		}
+	}
+	return nil
+}
+
+var (
+	discoveryManifest types.DiscoveryManifest
+	recipeCache       []types.OpenInstallationRecipe
+	repository        *recipes.RecipeRepository
+)
+
+func Setup() {
+	discoveryManifest = types.DiscoveryManifest{
+		OS: "linux",
+	}
+	recipeCache = []types.OpenInstallationRecipe{}
+	repository = recipes.NewRecipeRepository(recipeLoader, &discoveryManifest)
+}
+
+func givenBundler() *Bundler {
+	return NewBundler(repository)
+}
+
+func recipeLoader() ([]types.OpenInstallationRecipe, error) {
+	return recipeCache, nil
+}
+
+func givenRecipe(id string, name string) *types.OpenInstallationRecipe {
+	r := &types.OpenInstallationRecipe{
+		ID:   id,
+		Name: name,
+	}
+	t := types.OpenInstallationRecipeInstallTarget{
+		Os: "linux",
+	}
+	r.InstallTargets = append(r.InstallTargets, t)
+	recipeCache = append(recipeCache, *r)
+	return r
 }
