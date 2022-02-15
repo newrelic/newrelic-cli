f4d6f5f3f6f8509aa65901e503d72160df33e505
Julien Erard <jerard@newrelic.com>
2022-02-15 08:28:52 -0800

    chore(install): should only add recipe to bundle once

--- internal/install/recipes/bundle.go
+++ internal/install/recipes/bundle.go
@@ -1,43 +1,14 @@
 package recipes
 
-import (
-	"github.com/newrelic/newrelic-cli/internal/install/types"
-)
-
 type Bundle struct {
 	BundleRecipes []*BundleRecipe
 }
 
 func (b *Bundle) AddRecipe(bundleRecipe *BundleRecipe) {
-
-	if index := b.IndexOf(bundleRecipe.Recipe.Name); index != -1 {
-		b.BundleRecipes[index] = bundleRecipe
-	} else {
-		b.BundleRecipes = append(b.BundleRecipes, bundleRecipe)
+	if b.ContainsName(bundleRecipe.Recipe.Name) {
+		return
 	}
-}
-
-//TODO: do we need an IndexOf?
-func (b *Bundle) IndexOf(name string) int {
-	for i := range b.BundleRecipes {
-		if b.BundleRecipes[i].Recipe.Name == name {
-			return i
-		}
-	}
-
-	return -1
-}
-
-//TODO: Not sure if ths is needed
-func (b *Bundle) Contains(recipe *types.OpenInstallationRecipe) bool {
-
-	for i := range b.BundleRecipes {
-		if b.BundleRecipes[i].Recipe == recipe {
-			return true
-		}
-	}
-
-	return false
+	b.BundleRecipes = append(b.BundleRecipes, bundleRecipe)
 }
 
 func (b *Bundle) ContainsName(name string) bool {
