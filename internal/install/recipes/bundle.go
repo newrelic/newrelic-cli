c13a9042a971dd5bc4be14de0bf5366c160cd33f
Julien Erard <jerard@newrelic.com>
2022-02-15 08:02:27 -0800

    chore(install): split bundle_recipe in its own file

--- internal/install/recipes/bundle.go
+++ internal/install/recipes/bundle.go
@@ -1,16 +1,9 @@
 package recipes
 
 import (
-	"github.com/newrelic/newrelic-cli/internal/install/execution"
 	"github.com/newrelic/newrelic-cli/internal/install/types"
 )
 
-type BundleRecipe struct {
-	Recipe       *types.OpenInstallationRecipe
-	Dependencies []*BundleRecipe
-	//maybe timestamp
-	Statuses []execution.RecipeStatusType
-}
 type Bundle struct {
 	BundleRecipes []*BundleRecipe
 }
@@ -22,25 +15,6 @@ func (b *Bundle) AddRecipe(bundleRecipe *BundleRecipe) {
 	} else {
 		b.BundleRecipes = append(b.BundleRecipes, bundleRecipe)
 	}
-}
-
-func (br *BundleRecipe) AddStatus(status execution.RecipeStatusType) {
-	if br.HasStatus(status) {
-		return
-	}
-	if status == execution.RecipeStatusTypes.AVAILABLE {
-		br.Statuses = append(br.Statuses, execution.RecipeStatusTypes.DETECTED)
-	}
-	br.Statuses = append(br.Statuses, status)
-}
-
-func (br *BundleRecipe) HasStatus(status execution.RecipeStatusType) bool {
-	for _, value := range br.Statuses {
-		if value == status {
-			return true
-		}
-	}
-	return false
 }
 
 //TODO: do we need an IndexOf?
