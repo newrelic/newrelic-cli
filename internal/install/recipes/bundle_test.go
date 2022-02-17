10cb8ff41459807657f509f1276ec572de5f5d5d
Julien Erard <jerard@newrelic.com>
2022-02-17 08:40:37 -0800

    chore(install): refactor building of recipes in test

--- internal/install/recipes/bundle_test.go
+++ internal/install/recipes/bundle_test.go
@@ -6,60 +6,59 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-var (
-	bundleRecipe = &BundleRecipe{
-		Recipe: createRecipe("0", "recipe1"),
-	}
-	bundleRecipes = []*BundleRecipe{
-		{
-			Recipe: createRecipe("0", "recipe1"),
-		},
-		{
-			Recipe: createRecipe("1", "recipe2"),
-		},
-	}
-)
-
 func TestBundle_ShouldAddRecipes(t *testing.T) {
-
 	bundle := Bundle{
-		BundleRecipes: bundleRecipes,
+		BundleRecipes: []*BundleRecipe{
+			{
+				Recipe: NewRecipeBuilder().ID("0").Name("recipe1").Build(),
+			},
+		},
 	}
 	newRecipe := &BundleRecipe{
-		Recipe: createRecipe("2", "recipe3"),
+		Recipe: NewRecipeBuilder().ID("2").Name("recipe3").Build(),
 	}
 
 	bundle.AddRecipe(newRecipe)
-	require.Equal(t, len(bundle.BundleRecipes), len(bundleRecipes)+1)
+	require.Equal(t, len(bundle.BundleRecipes), 2)
 	require.True(t, true, bundle.ContainsName(newRecipe.Recipe.Name))
 }
 
 func TestBundle_ShouldNotUpdateRecipe(t *testing.T) {
-
+	bundleRecipe := &BundleRecipe{
+		Recipe: NewRecipeBuilder().ID("0").Name("recipe1").Build(),
+	}
 	bundle := Bundle{
-		BundleRecipes: bundleRecipes,
+		BundleRecipes: []*BundleRecipe{bundleRecipe},
 	}
 
 	bundle.AddRecipe(bundleRecipe)
 	bundle.AddRecipe(bundleRecipe)
 	bundle.AddRecipe(bundleRecipe)
 
-	require.Equal(t, len(bundle.BundleRecipes), len(bundleRecipes))
+	require.Equal(t, len(bundle.BundleRecipes), 1)
 	require.Equal(t, true, bundle.ContainsName(bundleRecipe.Recipe.Name))
 }
 
 func TestBundle_ShouldContainRecipeName(t *testing.T) {
 	bundle := Bundle{
-		BundleRecipes: bundleRecipes,
+		BundleRecipes: []*BundleRecipe{
+			{
+				Recipe: NewRecipeBuilder().ID("0").Name("recipe1").Build(),
+			},
+		},
 	}
 
-	require.Equal(t, true, bundle.ContainsName(bundleRecipe.Recipe.Name))
+	require.Equal(t, true, bundle.ContainsName("recipe1"))
 }
 
 func TestBundle_ShouldNotContainRecipeName(t *testing.T) {
 	bundle := Bundle{
-		BundleRecipes: bundleRecipes,
+		BundleRecipes: []*BundleRecipe{
+			{
+				Recipe: NewRecipeBuilder().ID("0").Name("recipe1").Build(),
+			},
+		},
 	}
 
-	require.Equal(t, false, bundle.ContainsName("some name"))
+	require.Equal(t, false, bundle.ContainsName("some other name"))
 }
