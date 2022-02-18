70cbddb2d64bce21e95bb7c5a2c3f6e44ebb2a74
Mike Bruzina <mbruzina@newrelic.com>
2022-02-18 16:06:02 -0600

    chore(recipestatus): remove timestamp from recipestatus

--- internal/install/recipes/bundle_recipe_test.go
+++ internal/install/recipes/bundle_recipe_test.go
@@ -10,45 +10,36 @@ import (
 )
 
 func TestBundleRecipeAddsStatusWithTime(t *testing.T) {
-	expectedStatusTime := time.Now()
 	expectedStatus := execution.RecipeStatusTypes.INSTALLING
 	br := testBundleRecipe()
 
-	br.AddStatus(expectedStatus, expectedStatusTime)
+	br.AddStatus(expectedStatus)
 
-	require.Equal(t, len(br.RecipeStatuses), 1)
-	require.Equal(t, expectedStatus, br.RecipeStatuses[0].Status)
-	require.Equal(t, expectedStatusTime, br.RecipeStatuses[0].StatusTime)
-
+	require.Equal(t, len(br.DetectedStatuses), 1)
+	require.Equal(t, expectedStatus, br.DetectedStatuses[0])
 }
 
 func TestBundleRecipeShouldAddStatusOnceAtFirstOccurrence(t *testing.T) {
 	br := testBundleRecipe()
-	earliestTime := time.Now()
-	moreRecentTime := earliestTime.Add(5 * time.Minute)
-	mostRecentTime := earliestTime.Add(10 * time.Minute)
 	expectedStatus := execution.RecipeStatusTypes.INSTALLING
 
-	br.AddStatus(expectedStatus, earliestTime)
-	br.AddStatus(expectedStatus, moreRecentTime)
-	br.AddStatus(expectedStatus, mostRecentTime)
+	br.AddStatus(expectedStatus)
+	br.AddStatus(expectedStatus)
+	br.AddStatus(expectedStatus)
 
-	require.Equal(t, len(br.RecipeStatuses), 1)
-	require.Equal(t, expectedStatus, br.RecipeStatuses[0].Status)
-	require.Equal(t, earliestTime, br.RecipeStatuses[0].StatusTime)
+	require.Equal(t, len(br.DetectedStatuses), 1)
+	require.Equal(t, expectedStatus, br.DetectedStatuses[0])
 }
 
 func TestBundleRecipeShouldAddStatusDetectedWhenAvailable(t *testing.T) {
 	br := testBundleRecipe()
-	expectedStatusTime := time.Now()
 
-	br.AddStatus(execution.RecipeStatusTypes.AVAILABLE, expectedStatusTime)
+	br.AddStatus(execution.RecipeStatusTypes.AVAILABLE)
 
-	require.Equal(t, len(br.RecipeStatuses), 2)
-	require.Equal(t, execution.RecipeStatusTypes.DETECTED, br.RecipeStatuses[0].Status)
-	require.Equal(t, expectedStatusTime, br.RecipeStatuses[0].StatusTime)
-	require.Equal(t, execution.RecipeStatusTypes.AVAILABLE, br.RecipeStatuses[1].Status)
-	require.Equal(t, expectedStatusTime, br.RecipeStatuses[1].StatusTime)
+	require.Equal(t, len(br.DetectedStatuses), 2)
+	require.Equal(t, execution.RecipeStatusTypes.DETECTED, br.DetectedStatuses[0])
+	require.Equal(t, execution.RecipeStatusTypes.AVAILABLE, br.DetectedStatuses[1])
+
 }
 
 func TestBundleRecipeHasStatusReturnsTrue(t *testing.T) {
@@ -108,10 +99,8 @@ func testBundleRecipe() *BundleRecipe {
 
 func testBundleRecipeWithStatus(status execution.RecipeStatusType, statusTime time.Time) *BundleRecipe {
 	bundleRecipe := testBundleRecipe()
-	bundleRecipe.RecipeStatuses = []RecipeStatus{{
-		Status:     status,
-		StatusTime: statusTime,
-	},
+	bundleRecipe.DetectedStatuses = []execution.RecipeStatusType{
+		status,
 	}
 	return bundleRecipe
 }
