bc091c3509d365a6b57340f80161c0d5f87e06ce
Sander Blue <sblue@newrelic.com>
2022-02-23 13:29:04 -0600

    chore(release): attempt release-windows-installer fix

--- build/package/msi/NewRelicCLIInstaller/SignMSI.cmd
+++ build/package/msi/NewRelicCLIInstaller/SignMSI.cmd
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64\SignTool.exe" sign /f .\build\package\msi\NewRelicCLIInstaller\developer-toolkit.pfx /p "%PFX_PASSWORD%" /t http://timestamp.digicert.com .\build\package\msi\NewRelicCLIInstaller\bin\x64\Release\NewRelicCLIInstaller.msi
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64\SignTool.exe" sign /f developer-toolkit.pfx /p "%PFX_PASSWORD%" /t http://timestamp.digicert.com bin\x64\Release\NewRelicCLIInstaller.msi
