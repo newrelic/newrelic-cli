6260c96f3b388511be7f8d46c1964bb6a80e7636
Sander Blue <sblue@newrelic.com>
2022-03-28 17:07:46 -0500

    fix(release): fix path to .pfx file in SignPS1.cmd file

--- build/package/msi/NewRelicCLIInstaller/SignPS1.cmd
+++ build/package/msi/NewRelicCLIInstaller/SignPS1.cmd
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64\SignTool.exe" sign /f developer-toolkit.pfx /p "%PFX_PASSWORD%" /t http://timestamp.digicert.com scripts\install.ps1
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64\SignTool.exe" sign /f bin\x64\Release\developer-toolkit.pfx /p "%PFX_PASSWORD%" /t http://timestamp.digicert.com scripts\install.ps1
