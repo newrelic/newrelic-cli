40066f75a99a2a2dd386d8e09846e2855c26d64d
Julien Erard <jerard@newrelic.com>
2022-04-29 16:57:51 -0700

    fix(install): add recipe installer test

--- internal/install/execution/mock_recipe_executor.go
+++ internal/install/execution/mock_recipe_executor.go
@@ -2,16 +2,21 @@ package execution
 
 import (
 	"context"
+	"encoding/json"
+	"log"
 
 	"github.com/newrelic/newrelic-cli/internal/install/types"
 )
 
 type MockRecipeExecutor struct {
-	ExecuteErr error
+	ExecuteErr   error
+	OutputParser *OutputParser
 }
 
 func NewMockRecipeExecutor() *MockRecipeExecutor {
-	return &MockRecipeExecutor{}
+	return &MockRecipeExecutor{
+		OutputParser: NewOutputParser(map[string]interface{}{}),
+	}
 }
 
 func (m *MockRecipeExecutor) Execute(ctx context.Context, r types.OpenInstallationRecipe, v types.RecipeVars) error {
@@ -23,5 +28,16 @@ func (m *MockRecipeExecutor) ExecutePreInstall(ctx context.Context, r types.Open
 }
 
 func (m *MockRecipeExecutor) GetOutput() *OutputParser {
-	return NewOutputParser(map[string]interface{}{})
+	return m.OutputParser
+}
+
+func (m *MockRecipeExecutor) SetOutput(value string) {
+	if value != "" {
+		var values map[string]interface{}
+		if err := json.Unmarshal([]byte(value), &values); err == nil {
+			m.OutputParser = NewOutputParser(values)
+			return
+		}
+		log.Fatalf("couldn't unmarshal json for mock with %s", value)
+	}
 }
