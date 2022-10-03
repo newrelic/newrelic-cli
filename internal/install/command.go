451eccc8c4d5b021664a3c0c0fd844a136aeb61f
Mike Bruzina <mbruzina@newrelic.com>
2022-10-03 16:24:31 -0500

    chore(logs): send error logs back using api

--- internal/install/command.go
+++ internal/install/command.go
@@ -1,6 +1,7 @@
 package install
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
@@ -45,33 +46,36 @@ var Command = &cobra.Command{
 			return nil
 		}
 
-		i := NewRecipeInstaller(ic, client.NRClient)
+		NewRecipeInstaller(ic, client.NRClient)
 
 		// Run the install.
-		if err := i.Install(); err != nil {
-			if err == types.ErrInterrupt {
-				return nil
-			}
+		//if err := i.Install(); err != nil {
 
-			if _, ok := err.(*types.UpdateRequiredError); ok {
-				return nil
-			}
+		err = errors.New("some weird error")
 
-			if e, ok := err.(*nrErrors.PaymentRequiredError); ok {
-				return e
-			}
-
-			fallbackErrorMsg := fmt.Sprintf("\nWe encountered an issue during the installation: %s.", err)
-			fallbackHelpMsg := "If this problem persists, visit the documentation and support page for additional help here at https://docs.newrelic.com/docs/infrastructure/install-infrastructure-agent/get-started/requirements-infrastructure-agent/"
-
-			// In the extremely rare case we run into an uncaught error (e.g. no recipes found),
-			// we need to output something to user to sinc we probably haven't displayed anything yet.
-			fmt.Println(fallbackErrorMsg)
-			fmt.Println(fallbackHelpMsg)
-			fmt.Print("\n\n")
-
-			log.Debug(fallbackErrorMsg)
+		if err == types.ErrInterrupt {
+			return nil
 		}
+
+		if _, ok := err.(*types.UpdateRequiredError); ok {
+			return nil
+		}
+
+		if e, ok := err.(*nrErrors.PaymentRequiredError); ok {
+			return e
+		}
+
+		fallbackErrorMsg := fmt.Sprintf("\nWe encountered an issue during the installation: %s.", err)
+		fallbackHelpMsg := "If this problem persists, visit the documentation and support page for additional help here at https://docs.newrelic.com/docs/infrastructure/install-infrastructure-agent/get-started/requirements-infrastructure-agent/"
+
+		// In the extremely rare case we run into an uncaught error (e.g. no recipes found),
+		// we need to output something to user to sinc we probably haven't displayed anything yet.
+		fmt.Println(fallbackErrorMsg)
+		fmt.Println(fallbackHelpMsg)
+		fmt.Print("\n\n")
+
+		log.Debug(fallbackErrorMsg)
+		//}
 
 		return nil
 	},
