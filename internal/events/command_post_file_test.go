005fcdfd520060cf178cd41558e9aca85b32c831
Julien Erard <jerard@newrelic.com>
2023-02-21 15:10:07 -0800

    fix(events): fix lint

--- internal/events/command_post_file_test.go
+++ internal/events/command_post_file_test.go
@@ -16,28 +16,28 @@ func TestPostFile(t *testing.T) {
 }
 
 func TestShouldGetJSONArray(t *testing.T) {
-	myJsonString := `[{"key1":"value1"},{"key2":"value2"}]`
-	array := getArray([]byte(myJsonString))
+	input := `[{"key1":"value1"},{"key2":"value2"}]`
+	array := getArray([]byte(input))
 	assert.Equal(t, 2, len(*array))
 }
 
 func TestShouldNotGetArrayWhenJSON(t *testing.T) {
-	myJsonString := `{"some":"json"}`
-	array := getArray([]byte(myJsonString))
+	input := `{"some":"json"}`
+	array := getArray([]byte(input))
 	assert.Equal(t, 0, len(*array))
 }
 
 func TestShouldSliceBy(t *testing.T) {
-	myJsonString := `[{"key1":"value1"},{"key2":"value2"},{"key3":"value3"},{"key4":"value5"},{"key5":"value5"}]`
-	array := getArray([]byte(myJsonString))
+	input := `[{"key1":"value1"},{"key2":"value2"},{"key3":"value3"},{"key4":"value5"},{"key5":"value5"}]`
+	array := getArray([]byte(input))
 	sliced := sliceBy(array, 3)
 	assert.Equal(t, 3, len(sliced[0]))
 	assert.Equal(t, 2, len(sliced[1]))
 }
 
 func TestShouldSliceByMax(t *testing.T) {
-	myJsonString := `[{"key1":"value1"},{"key2":"value2"},{"key3":"value3"},{"key4":"value5"},{"key5":"value5"}]`
-	array := getArray([]byte(myJsonString))
+	input := `[{"key1":"value1"},{"key2":"value2"},{"key3":"value3"},{"key4":"value5"},{"key5":"value5"}]`
+	array := getArray([]byte(input))
 	sliced := sliceBy(array, 5)
 	assert.Equal(t, 5, len(sliced[0]))
 }
