6494fc3ef4b91e7c86f8b6a588e47486219d7b57
ravitejasurampudi <ravitejasurampudi@newrelic.com>
2023-03-06 23:30:56 +0530

    chore: added raw configuration propeties to dashboard

--- internal/utils/terraform/dashboard.go
+++ internal/utils/terraform/dashboard.go
@@ -2,7 +2,6 @@ package terraform
 
 import (
 	"encoding/json"
-	"strconv"
 	"strings"
 
 	log "github.com/sirupsen/logrus"
@@ -31,16 +30,17 @@ var (
 )
 
 type DashboardWidgetRawConfiguration struct {
-	DataFormatters    []DataFormatter            `json:"dataFormatters"`
-	NRQLQueries       []DashboardWidgetNRQLQuery `json:"nrqlQueries"`
-	LinkedEntityGUIDs []string                   `json:"linkedEntityGuids"`
-	Text              string                     `json:"text"`
-	Facet             DashboardWidgetFacet       `json:"facet"`
-	Legend            DashboardWidgetLegend      `json:"legend"`
-	YAxisLeft         DashboardWidgetYAxisLeft   `json:"yAxisLeft"`
-	NullValues        DashboardWidgetNullValues  `json:"nullValues"`
-	Units             DashboardWidgetUnits       `json:"units"`
-	Colors            DashboardWidgetColors      `json:"colors"`
+	DataFormatters    []DataFormatter                `json:"dataFormatters"`
+	NRQLQueries       []DashboardWidgetNRQLQuery     `json:"nrqlQueries"`
+	LinkedEntityGUIDs []string                       `json:"linkedEntityGuids"`
+	Text              string                         `json:"text"`
+	Facet             DashboardWidgetFacet           `json:"facet,omitempty"`
+	Legend            DashboardWidgetLegend          `json:"legend,omitempty"`
+	YAxisLeft         DashboardWidgetYAxisLeft       `json:"yAxisLeft,omitempty"`
+	NullValues        DashboardWidgetNullValues      `json:"nullValues,omitempty"`
+	Units             DashboardWidgetUnits           `json:"units,omitempty"`
+	Colors            DashboardWidgetColors          `json:"colors,omitempty"`
+	PlatformOptions   DashboardWidgetPlatformOptions `json:"platformOptions,omitempty"`
 }
 
 type DataFormatter struct {
@@ -59,40 +59,44 @@ type DashboardWidgetNRQLQuery struct {
 }
 
 type DashboardWidgetLegend struct {
-	Enabled bool `json:"enabled"`
+	Enabled bool `json:"enabled,omitempty"`
 }
 
 type DashboardWidgetYAxisLeft struct {
-	Zero bool `json:"zero"`
+	Max float64 `json:"max,omitempty"`
+	Min float64 `json:"min,omitempty"`
 }
 
 type DashboardWidgetNullValues struct {
-	NullValue       string                              `json:"nullValue"`
-	SeriesOverrides []DashboardWidgetNullValueOverrides `json:"seriesOverrides"`
+	NullValue       string                              `json:"nullValue,omitempty"`
+	SeriesOverrides []DashboardWidgetNullValueOverrides `json:"seriesOverrides,omitempty"`
 }
 
 type DashboardWidgetNullValueOverrides struct {
-	NullValue  string `json:"nullValue"`
-	SeriesName string `json:"seriesName"`
+	NullValue  string `json:"nullValue,omitempty"`
+	SeriesName string `json:"seriesName,omitempty"`
 }
 type DashboardWidgetUnits struct {
-	Unit            string                         `json:"unit"`
-	SeriesOverrides []DashboardWidgetUnitOverrides `json:"seriesOverrides"`
+	Unit            string                         `json:"unit,omitempty"`
+	SeriesOverrides []DashboardWidgetUnitOverrides `json:"seriesOverrides,omitempty"`
 }
 
 type DashboardWidgetUnitOverrides struct {
-	Unit       string `json:"unit"`
-	SeriesName string `json:"seriesName"`
+	Unit       string `json:"unit,omitempty"`
+	SeriesName string `json:"seriesName,omitempty"`
 }
 
 type DashboardWidgetColors struct {
-	Color           string                          `json:"color"`
-	SeriesOverrides []DashboardWidgetColorOverrides `json:"seriesOverrides"`
+	Color           string                          `json:"color,omitempty"`
+	SeriesOverrides []DashboardWidgetColorOverrides `json:"seriesOverrides,omitempty"`
 }
 
 type DashboardWidgetColorOverrides struct {
-	Color      string `json:"color"`
-	SeriesName string `json:"seriesName"`
+	Color      string `json:"color,omitempty"`
+	SeriesName string `json:"seriesName,omitempty"`
+}
+type DashboardWidgetPlatformOptions struct {
+	IgnoreTimeRange bool `json:"ignoreTimeRange,omitempty"`
 }
 
 func GenerateDashboardHCL(resourceLabel string, shiftWidth int, input []byte) (string, error) {
@@ -133,39 +137,35 @@ func GenerateDashboardHCL(resourceLabel string, shiftWidth int, input []byte) (s
 								h.WriteMultilineStringAttribute("query", q.Query)
 							})
 						}
-						h.WriteBlock("facet", []string{}, func() {
-							h.WriteStringAttribute("showOtherSeries", strconv.FormatBool(config.Facet.ShowOtherSeries))
-						})
-						h.WriteBlock("legend", []string{}, func() {
-							h.WriteStringAttribute("enabled", strconv.FormatBool(config.Legend.Enabled))
-						})
-						h.WriteBlock("yAxisLeft", []string{}, func() {
-							h.WriteStringAttribute("zero", strconv.FormatBool(config.YAxisLeft.Zero))
-						})
-						h.WriteBlock("nullValues", []string{}, func() {
-							h.WriteStringAttribute("nullValue", config.NullValues.NullValue)
+						h.WriteBooleanAttribute("facet_show_other_series", config.Facet.ShowOtherSeries)
+						h.WriteBooleanAttribute("legend_enabled", config.Legend.Enabled)
+						h.WriteBooleanAttribute("ignore_time_range", config.PlatformOptions.IgnoreTimeRange)
+						h.WriteFloatAttribute("y_axis_left_min", config.YAxisLeft.Min)
+						h.WriteFloatAttribute("y_axis_left_max", config.YAxisLeft.Max)
+						h.WriteBlock("null_values", []string{}, func() {
+							h.WriteStringAttribute("null_value", config.NullValues.NullValue)
 							for _, so := range config.NullValues.SeriesOverrides {
-								h.WriteBlock("seriesOverrides", []string{}, func() {
-									h.WriteStringAttribute("nullValue", so.NullValue)
-									h.WriteStringAttribute("seriesName", so.SeriesName)
+								h.WriteBlock("series_overrides", []string{}, func() {
+									h.WriteStringAttribute("null_value", so.NullValue)
+									h.WriteStringAttribute("series_name", so.SeriesName)
 								})
 							}
 						})
 						h.WriteBlock("units", []string{}, func() {
 							h.WriteStringAttribute("unit", config.Units.Unit)
 							for _, so := range config.Units.SeriesOverrides {
-								h.WriteBlock("seriesOverrides", []string{}, func() {
+								h.WriteBlock("series_overrides", []string{}, func() {
 									h.WriteStringAttribute("unit", so.Unit)
-									h.WriteStringAttribute("seriesName", so.SeriesName)
+									h.WriteStringAttribute("series_name", so.SeriesName)
 								})
 							}
 						})
 						h.WriteBlock("colors", []string{}, func() {
 							h.WriteStringAttribute("color", config.Colors.Color)
 							for _, so := range config.Colors.SeriesOverrides {
-								h.WriteBlock("seriesOverrides", []string{}, func() {
+								h.WriteBlock("series_overrides", []string{}, func() {
 									h.WriteStringAttribute("color", so.Color)
-									h.WriteStringAttribute("seriesName", so.SeriesName)
+									h.WriteStringAttribute("series_name", so.SeriesName)
 								})
 							}
 						})
