07c359675832f14654deeb5457f963cf4c16e53f
Julien4218 <jerard@newrelic.com>
2023-05-19 17:00:22 -0700

    fix(install): swap segment client to use http api

--- internal/install/segment/client_test.go
+++ internal/install/segment/client_test.go
@@ -1,35 +1,41 @@
 package segment
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"strconv"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/newrelic/newrelic-cli/internal/install/types"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestClientShouldInit(t *testing.T) {
-	mc := &MockSegmentClient{}
+	server := initSegmentMockServer()
+	defer server.Close()
+	baseUrl := server.URL
 	accoundID := 12345
 	region := "STAGING"
+	writeKey := "secretWriteKey"
 
-	c := NewWithClient(mc, accoundID, region, false)
+	c := NewWithUrl(baseUrl, writeKey, accoundID, region, false)
 	assert.NotNil(t, c, "Segment client should create, and not return nil")
 }
 
 func TestClientShouldTrack(t *testing.T) {
-	mc := &MockSegmentClient{}
+	server := initSegmentMockServer()
+	defer server.Close()
+	baseUrl := server.URL
 	accoundID := 12345
 	region := "STAGING"
+	writeKey := "secretWriteKey"
 	installID := "installID123"
 
-	c := NewWithClient(mc, accoundID, region, true)
+	c := NewWithUrl(baseUrl, writeKey, accoundID, region, true)
 	c.SetInstallID(installID)
 	tResult := c.Track(types.EventTypes.APIKeyMissing)
 	userID, _ := strconv.Atoi(tResult.UserId)
 
-	assert.Equal(t, 1, mc.EnqueueCallCount, "Segment should call enqueue one time when track one time")
 	assert.Equal(t, accoundID, userID)
 	assert.Equal(t, region, tResult.Properties["region"])
 	assert.Equal(t, installID, tResult.Properties["installID"])
@@ -39,15 +45,25 @@ func TestClientShouldTrack(t *testing.T) {
 }
 
 func TestClientShouldTrackInfo(t *testing.T) {
-	mc := &MockSegmentClient{}
+	server := initSegmentMockServer()
+	defer server.Close()
+	baseUrl := server.URL
 	accoundID := 12345
 	region := "STAGING"
+	writeKey := "secretWriteKey"
 
 	ei := NewEventInfo(types.EventTypes.OtherError, "hello world")
 
-	c := NewWithClient(mc, accoundID, region, true)
+	c := NewWithUrl(baseUrl, writeKey, accoundID, region, true)
 	tResult := c.TrackInfo(ei)
 
-	assert.Equal(t, 1, mc.EnqueueCallCount, "Segment should call enqueue one time when track one time")
 	assert.Equal(t, "hello world", tResult.Properties["Detail"])
+}
+
+func initSegmentMockServer() *httptest.Server {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		_, _ = w.Write([]byte(`[]`))
+	}))
+	return server
 }
