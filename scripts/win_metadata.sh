f48bfb08148de12fce52e22dfd10c071e5bd795a
Alfredo Molero <amolero@newrelic.com>
2024-02-28 12:31:33 -0500

    chore(release): Try get git tag from within script

--- scripts/win_metadata.sh
+++ scripts/win_metadata.sh
@@ -7,8 +7,7 @@ VERSION_FILE=cmd/newrelic/versioninfo.json
 SYSO_FILE=cmd/newrelic/resource_windows.syso
 TPL_FILE=templates/versioning/versioninfo.json.template
 
-# $VERSION is defined in goreleaser's global 'env'
-VERSION=$1
+VERSION=$(git describe --tags --always --abbrev=0 | sed -e '/^v/s/^v\(.*\)$$/\1/g' | sed 's/v//g')
 echo "VERSION: $VERSION"
 
 # Get semver values.
