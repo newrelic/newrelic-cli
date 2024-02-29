0d95c2ba99aa948c9011ea9957a47ab540b3db99
Alfredo Molero <amolero@newrelic.com>
2024-02-29 15:59:17 -0500

    chore(release): Debug options clean up and add some comments

--- build/release.mk
+++ build/release.mk
@@ -21,13 +21,13 @@ release-clean:
 release-publish: clean tools docker-login release-notes recipes events
 	@echo "=== $(PROJECT_NAME) === [ release-publish  ]: Publishing release via $(REL_CMD)"
 	@cat $(SRCDIR)/tmp/$(RELEASE_NOTES_FILE) || true
-	$(REL_CMD) release --release-notes=$(SRCDIR)/tmp/$(RELEASE_NOTES_FILE) --debug
+	$(REL_CMD) release --release-notes=$(SRCDIR)/tmp/$(RELEASE_NOTES_FILE)
 
 # Local Snapshot
 snapshot: clean tools recipes events
 	@echo "=== $(PROJECT_NAME) === [ snapshot         ]: Creating release via $(REL_CMD)"
 	@echo "=== $(PROJECT_NAME) === [ snapshot         ]:   THIS WILL NOT BE PUBLISHED!"
-	@$(REL_CMD) --skip-publish --snapshot --debug
+	@$(REL_CMD) --skip-publish --snapshot
 
 release-homebrew:
 ifeq ($(HOMEBREW_GITHUB_API_TOKEN), "")
