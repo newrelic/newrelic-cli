083092cab193463a059c6b8642787c07aab38652
pranav-new-relic <127438038+pranav-new-relic@users.noreply.github.com>
2024-05-15 12:15:35 +0530

    chore: Revert "feat(entity): Entity guid validation" (#1605) (#1607)

--- internal/entities/command_deployment.go
+++ internal/entities/command_deployment.go
@@ -10,13 +10,14 @@ import (
 
 	"github.com/spf13/cobra"
 
-	"github.com/newrelic/newrelic-cli/internal/client"
+	"github.com/newrelic/newrelic-client-go/v2/pkg/nrtime"
+
 	"github.com/newrelic/newrelic-cli/internal/output"
 	"github.com/newrelic/newrelic-cli/internal/utils"
 	"github.com/newrelic/newrelic-client-go/v2/pkg/changetracking"
 	"github.com/newrelic/newrelic-client-go/v2/pkg/common"
-	"github.com/newrelic/newrelic-client-go/v2/pkg/entities"
-	"github.com/newrelic/newrelic-client-go/v2/pkg/nrtime"
+
+	"github.com/newrelic/newrelic-cli/internal/client"
 )
 
 var (
@@ -65,10 +66,6 @@ The deployment command marks a change for a New Relic entity
 
 		if version == "" {
 			log.Fatal("--version cannot be empty")
-		}
-
-		if _, err := entities.DecodeEntityGuid(entityGUID); err != nil {
-			log.Fatal(err)
 		}
 
 		var (
