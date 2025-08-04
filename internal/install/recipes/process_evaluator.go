b6ffd7e024c1003305a8a43daddbca85eb00d381
shashank-reddy-nr <spulipelly@newrelic.com>
2025-08-04 14:39:10 +0530

    chore: added debug print

--- internal/install/recipes/process_evaluator.go
+++ internal/install/recipes/process_evaluator.go
@@ -60,6 +60,7 @@ func GetPsUtilCommandLines(ctx context.Context) []types.GenericProcess {
 			continue
 		}
 		name, _ := psproc.Name()
+		log.Debugf("process name is: %s", name)
 		if name == "lsass.exe" {
 			log.Debugf("skipping sensitive process: %s", name)
 			continue
